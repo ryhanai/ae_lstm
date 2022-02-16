14f1bb76b8881e4bf43e4f69354848306e4b5741
Ryo Hanai <ryo.hanai@aist.go.jp>
2022-02-17 00:12:42 +0900

    prediction module that takes ROI parameter as an additional argument

--- scripts/roi_ae_lstm_v15.py
+++ scripts/roi_ae_lstm_v15.py
@@ -15,13 +15,14 @@ from core import trainer
 os.environ['TF_GPU_THREAD_MODE'] = 'gpu_private'
 
 
-dataset = 'peg-in-hole-no-shadow'
+dataset = 'reaching-no-shadow'
 train_groups=range(1,300)
 val_groups=range(300,350)
 input_image_size=(80,160)
 time_window_size=20
 latent_dim=32
 dof=7
+batch_size=32
 
 roi_size = (40, 80)
 
@@ -72,7 +73,7 @@ class AutoencoderWithCrop(tf.keras.Model):
     """
     def __init__(self, *args, **kargs):
         super(AutoencoderWithCrop, self).__init__(*args, **kargs)
-        tracker_names = ['image_loss', 'joint_loss', 'scale', 'loss']
+        tracker_names = ['image_loss', 'joint_loss', 'loss']
         self.train_trackers = {}
         self.test_trackers = {}
         for n in tracker_names:
@@ -82,9 +83,14 @@ class AutoencoderWithCrop(tf.keras.Model):
     def train_step(self, data):
         x, y = data
 
+        roi_pos = tf.random.uniform([batch_size, 2])
+        roi_scale = tf.random.uniform([batch_size])
+        roi_param = tf.concat([roi_pos, tf.expand_dims(roi_scale, 1)], 1)
+        rect = roi_rect1([roi_pos, roi_scale])
+
         with tf.GradientTape() as tape:
-            y_pred = self(x, training=True) # Forward pass
-            image_loss, joint_loss, scale, loss = self.compute_loss(y, y_pred, x)
+            y_pred = self([x[0],x[1],roi_param], training=True) # Forward pass
+            image_loss, joint_loss, loss = self.compute_loss(y, y_pred, x, rect)
 
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
@@ -92,32 +98,36 @@ class AutoencoderWithCrop(tf.keras.Model):
 
         self.train_trackers['image_loss'].update_state(image_loss)
         self.train_trackers['joint_loss'].update_state(joint_loss)
-        self.train_trackers['scale'].update_state(scale)
         self.train_trackers['loss'].update_state(loss)
         return dict([(trckr[0], trckr[1].result()) for trckr in self.train_trackers.items()])
 
     def test_step(self, data):
         x, y = data
-        y_pred = self(x, training=False) # Forward pass
-        image_loss, joint_loss, scale, loss = self.compute_loss(y, y_pred, x)
+
+        roi_pos = tf.random.uniform([batch_size, 2])
+        roi_scale = tf.random.uniform([batch_size])
+        roi_param = tf.concat([roi_pos, tf.expand_dims(roi_scale, 1)], 1)
+        rect = roi_rect1([roi_pos, roi_scale])
+
+        y_pred = self([x[0],x[1],roi_param], training=False) # Forward pass
+        image_loss, joint_loss, loss = self.compute_loss(y, y_pred, x, rect)
 
         self.test_trackers['image_loss'].update_state(image_loss)
         self.test_trackers['joint_loss'].update_state(joint_loss)
-        self.test_trackers['scale'].update_state(scale)
         self.test_trackers['loss'].update_state(loss)
         return dict([(trckr[0], trckr[1].result()) for trckr in self.test_trackers.items()])
 
-    def compute_loss(self, y, y_pred, x):
+    def compute_loss(self, y, y_pred, x, rect):
         x_image, x_joint = x
         y_image, y_joint = y
 
-        image_loss = tf.reduce_mean(tf.square(y_image - y_pred[0]))
+        y_cropped = crop_and_resize((y_image, rect))
+                          
+        image_loss = tf.reduce_mean(tf.square(y_cropped - y_pred[0]))
         joint_loss = tf.reduce_mean(tf.square(y_joint - y_pred[1]))
-        roi = y_pred[2][0,-1]
-        s = roi[2] - roi[0]
-        scale = s
+
         loss = image_loss + joint_loss
-        return image_loss, joint_loss, scale, loss
+        return image_loss, joint_loss, loss
 
     @property
     def metrics(self):
@@ -151,104 +161,60 @@ def model_lstm(time_window_size, image_vec_dim, dof, lstm_units=50, use_stacked_
     m.summary()
     return m
 
-def model_roi_ae_lstm(input_image_shape, time_window_size, image_vec_dim, dof):
+def model_lstm_no_split(time_window_size, image_vec_dim, dof, lstm_units=50, use_stacked_lstm=False, name='lstm'):
+    imgvec_input = tf.keras.Input(shape=(time_window_size, image_vec_dim))
+    joint_input = tf.keras.Input(shape=(time_window_size, dof))
+    state_dim = image_vec_dim + dof
+    x = tf.keras.layers.concatenate([imgvec_input, joint_input])
+
+    if use_stacked_lstm:
+        x = tf.keras.layers.LSTM(state_dim, return_sequences=True)(x)
+        x = tf.keras.layers.LSTM(state_dim, return_sequences=True)(x)
+
+    x = tf.keras.layers.LSTM(lstm_units)(x)
+    x = tf.keras.layers.Dense(state_dim)(x)
+
+    lstm = keras.Model([imgvec_input, joint_input], x, name=name)
+    lstm.summary()
+    return lstm
+
+def model_prediction(input_image_shape, time_window_size, image_vec_dim, dof):
     image_input = tf.keras.Input(shape=((time_window_size,) + input_image_shape))
     joint_input = tf.keras.Input(shape=(time_window_size, dof))
+    roi_input = tf.keras.Input(shape=(3,))
 
-    encoded_img = model_encoder(input_image_shape, time_window_size, image_vec_dim)(image_input)
-    predicted_ivec, predicted_jvec, roi =  model_lstm(time_window_size, image_vec_dim, dof)([encoded_img, joint_input])
+    # convert an ROI param to a rectangular region
+    center = tf.keras.layers.Lambda(lambda x:x[:,:2], output_shape=(2,))(roi_input)
+    scale = tf.keras.layers.Lambda(lambda x:x[:,2], output_shape=(1,))(roi_input)
+    rect = tf.keras.layers.Lambda(roi_rect)([center, scale])
 
-    decoded_roi = model_decoder(input_image_shape, image_vec_dim)(predicted_ivec)
-    last_frame = tf.keras.layers.Lambda(lambda x:x[:,-1], output_shape=())(image_input)
-    embedded_img = tf.keras.layers.Lambda(embed)([last_frame, decoded_roi, roi])
+    # crop&resize
+    roi_img = tf.keras.layers.TimeDistributed(tf.keras.layers.Lambda(crop_and_resize, output_shape=input_image_shape)) ([image_input, rect])
 
-    m = AutoencoderWithCrop(inputs=[image_input, joint_input],
-                            outputs=[embedded_img, predicted_jvec, roi],
-                            name='roi_ae_lstm_embed')
-    m.summary()
-    return m
+    # encode
+    roi_encoding = model_encoder(input_image_shape, time_window_size, image_vec_dim)(roi_img)
 
-def conv1_block(x, out_channels):
-    x = tf.keras.layers.Conv2D(out_channels, kernel_size=3, strides=1, padding='same', activation='selu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    return tf.keras.layers.MaxPool2D(pool_size=2)(x)
+    # LSTM
+    predicted_state = model_lstm_no_split(time_window_size, image_vec_dim, dof)([roi_encoding, joint_input])
 
-def model_encoder1(input_shape, out_dim, noise_stddev=0.2, name='encoder'):
-    image_input = tf.keras.Input(shape=(input_shape,))
+    # separete predicted 
+    predicted_roi_encoding = tf.keras.layers.Lambda(lambda x:x[:,:image_vec_dim], output_shape=(image_vec_dim,))(predicted_state)
+    predicted_joint_positions = tf.keras.layers.Lambda(lambda x:x[:,image_vec_dim:], output_shape=(dof,))(predicted_state)
 
-    # Denoising Autoencoder
-    x = tf.keras.layers.GaussianNoise(noise_stddev)(image_input)
+    # decode
+    predicted_roi = model_decoder(input_image_shape, image_vec_dim)(predicted_roi_encoding)
 
-    x = conv1_block(x, 8)
-    x = conv1_block(x, 16)
-    x = conv1_block(x, 32)
-    x = conv1_block(x, 64)
+    #last_frame = tf.keras.layers.Lambda(lambda x:x[:,-1], output_shape=())(image_input)
+    #embedded_img = tf.keras.layers.Lambda(embed)([last_frame, decoded_roi, roi])
 
-    x = tf.keras.layers.Flatten()(x)
-    encoder_output = tf.keras.layers.Dense(out_dim, activation='selu')(x)
-
-    encoder = keras.Model([image_input], encoder_output, name=name)
-    encoder.summary()
-    return encoder
-
-def model_roi_decoder(output_shape, image_vec_dim, name='roi_decoder'):
-    channels = output_shape[2]
-    nblocks = 4
-    h = int(output_shape[0]/2**nblocks)
-    w = int(output_shape[1]/2**nblocks)
-
-    imgvec_input = tf.keras.Input(shape=(image_vec_dim))
-    roi_input = tf.keras.Input(shape=(3)) # x,y and scale
-
-    # concatenate image vector and roi vector
-    concat_vec = tf.keras.layers.concatenate([imgvec_input, roi_input])
-
-    x = tf.keras.layers.Dense(h*w*channels, activation='selu')(concat_vec)
-    x = tf.keras.layers.BatchNormalization()(x)
-
-    x = tf.keras.layers.Reshape(target_shape=(h, w, channels))(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-
-    x = deconv_block(x, 64)
-    x = deconv_block(x, 32)
-    x = deconv_block(x, 16)
-    x = deconv_block(x, 8)
-
-    decoder_output = tf.keras.layers.Conv2DTranspose(3, kernel_size=3, strides=1, padding='same', activation='sigmoid')(x)
-    decoder = keras.Model(imgvec_input, decoder_output, name=name)
-    decoder.summary()
-    return decoder
-
-def model_roi_ae(input_shape, image_vec_dim, name='roi_ae'):
-    image_input = tf.keras.Input(shape=(input_image_shape))
-    roi_input = tf.keras.Input(shape=(2))
-
-    # encode the whole image
-    encoder = model_encoder1(input_shape, image_vec_dim)
-    image_vec = encoder(image_input)
-
-    # decode to the specifiec ROI image
-    decoder = model_roi_decoder(output_shape, image_vec_dim)
-    roi_image = decoder(image_vec, roi_input)
-
-    # returns the whole model together with encoder and decoder for the separate use
-    roi_ae = keras.Model([image_input, roi_input], [roi_image], name=name)
-
-    return encoder, decoder, roi_ae
-
-def model_roi_ae_train(input_shape, image_vec_dim, name='roi_ae_train'):
-    image_input = tf.keras.Input(shape=(input_image_shape))
-    e, d, ae = model_roi_ae(input_shape, image_vec_dim)
-
-    roi_input = random()
-    roi_image = ae(image_input, roi_input)
-    m = keras.Model(image_input, [roi_image, roi_input], name=name)
+    m = AutoencoderWithCrop(inputs=[image_input, joint_input, roi_input],
+                            outputs=[predicted_roi, predicted_joint_positions],
+                            name='predictor')
     m.summary()
     return m
 
 
-
-model = model_roi_ae_lstm(input_image_size+(3,), time_window_size, latent_dim, dof)
+model = model_prediction(input_image_size+(3,), time_window_size, latent_dim, dof)
 
 def train():
     train_ds = Dataset(dataset)
@@ -261,10 +227,31 @@ def train():
     tr.train()
     return tr
 
-def prepare_for_test(cp='ae_cp.peg-in-hole-no-shadow.roi_ae_lstm_embed.20220214120204'):
+class Predictor(trainer.Trainer):
+    def __init__(self, *args, **kargs):
+        super(Predictor, self).__init__(*args, **kargs)
+
+    def get_data(self):
+        return next(self.val_gen)
+        
+    def predict_with_roi(self, batch, roi_params):
+        x,y = batch
+        batch_sz = x[1].shape[0]
+        if roi_params.ndim == 1:
+            roi_params = np.tile(roi_params, (batch_sz,1))
+        predicted_images, predicted_joints = self.model.predict(x + (roi_params,))
+        #visualize_ds(y[0], roi_params) # to draw ROI roi_params must be converted to rectangle
+        visualize_ds(y[0])
+        visualize_ds(predicted_images)
+        plt.show()
+
+        
+def prepare_for_test(cp='ae_cp.reaching-no-shadow.predictor.20220216182028'):
     val_ds = Dataset(dataset)
     val_ds.load(groups=val_groups, image_size=input_image_size)
     val_ds.preprocess(time_window_size)
-    tr = trainer.Trainer(model, None, val_ds, time_window_size=time_window_size, checkpoint_file=cp)
+    tr = Predictor(model, None, val_ds, time_window_size=time_window_size, checkpoint_file=cp)
     return tr
 
+
+    
