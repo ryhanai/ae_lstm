7be1aef77e156a95139f52cde0ea66d38b6db2a4
Ryo Hanai <ryo.hanai@aist.go.jp>
2022-06-08 19:23:58 +0900

    mask-based attention model (static)

--- scripts/roi_static2.py
+++ scripts/roi_static2.py
@@ -74,7 +74,7 @@ def embed(args):
 class StaticAttentionEstimatorModel(tf.keras.Model):
     def __init__(self, *args, **kargs):
         super(StaticAttentionEstimatorModel, self).__init__(*args, **kargs)
-        tracker_names = ['loss']
+        tracker_names = ['reconst_loss', 'pred_loss', 'loss']
         self.train_trackers = {}
         self.test_trackers = {}
         for n in tracker_names:
@@ -88,12 +88,14 @@ class StaticAttentionEstimatorModel(tf.keras.Model):
 
         with tf.GradientTape() as tape:
             y_pred = self((x, input_noise), training=True) # Forward pass
-            loss = self.compute_loss(y, y_pred, input_noise)
+            rloss, ploss, loss = self.compute_loss(y, y_pred, input_noise)
 
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
+        self.train_trackers['reconst_loss'].update_state(rloss)
+        self.train_trackers['pred_loss'].update_state(ploss)
         self.train_trackers['loss'].update_state(loss)
         return dict([(trckr[0], trckr[1].result()) for trckr in self.train_trackers.items()])
 
@@ -103,16 +105,21 @@ class StaticAttentionEstimatorModel(tf.keras.Model):
         input_noise = tf.zeros(shape=(batch_size, 2))
 
         y_pred = self((x, input_noise), training=False) # Forward pass
-        loss = self.compute_loss(y, y_pred, input_noise)
+        rloss, ploss, loss = self.compute_loss(y, y_pred, input_noise)
 
+        self.test_trackers['reconst_loss'].update_state(rloss)
+        self.test_trackers['pred_loss'].update_state(ploss)
         self.test_trackers['loss'].update_state(loss)
         return dict([(trckr[0], trckr[1].result()) for trckr in self.test_trackers.items()])
 
     def compute_loss(self, y, y_pred, input_noise):
         y_aug = model.translate_image(y, input_noise)
         y_pred_img = y_pred[0]
-        loss = tf.reduce_mean(tf.square(y_aug - y_pred_img))
-        return loss
+        y_decoded_img = y_pred[1]
+        reconstruction_loss = tf.reduce_mean(tf.square(y_aug - y_decoded_img))
+        prediction_loss = tf.reduce_mean(tf.square(y_aug - y_pred_img))
+        loss = reconstruction_loss + prediction_loss
+        return reconstruction_loss, prediction_loss, loss
 
     @property
     def metrics(self):
@@ -124,14 +131,14 @@ def conv_block(x, out_channels):
     x = tf.keras.layers.BatchNormalization()(x)
     return tf.keras.layers.MaxPool2D(pool_size=2)(x)
 
-def model_encoder(input_shape, noise_stddev=0.2, name='encoder'):
+def model_encoder(input_shape, name='encoder'):
     image_input = tf.keras.Input(shape=(input_shape))
     
     x = conv_block(image_input, 8)
     x = conv_block(x, 16)
     x = conv_block(x, 32)
     x = conv_block(x, 64)
-
+    
     encoder = keras.Model([image_input], x, name=name)
     encoder.summary()
     return encoder
@@ -146,11 +153,8 @@ def model_decoder(output_shape, name='decoder'):
     nblocks = 4
     h = int(output_shape[0]/2**nblocks)
     w = int(output_shape[1]/2**nblocks)
-    
+        
     input_feature = tf.keras.Input(shape=(h, w, 64))
-    # x = tf.keras.layers.Dense(h*w*64, activation='selu')(input_feature)
-    # x = tf.keras.layers.BatchNormalization()(x)
-
     x = deconv_block(input_feature, 64)
     x = deconv_block(x, 32)
     x = deconv_block(x, 16)
@@ -159,14 +163,11 @@ def model_decoder(output_shape, name='decoder'):
     decoded_img = tf.keras.layers.Conv2DTranspose(3, kernel_size=3, strides=1, padding='same', activation='sigmoid')(x)
     # Need BatchNormalization here?
     
-    # decoded_roi_params = tf.keras.layers.Dense(6, activation='selu')(x)
-    # decoder = keras.Model(input_feature, [decoded_roi_images, decoded_roi_params], name=name)
-
     decoder = keras.Model([input_feature], decoded_img, name=name)    
     decoder.summary()
     return decoder
 
-def model_roi_ae(input_image_shape, name='roi_ae'):
+def model_ae(input_image_shape, name='autoencoder'):
     image_input = tf.keras.Input(shape=(input_image_shape))
 
     encoded_img = model_encoder(input_image_shape)(image_input)
@@ -191,11 +192,11 @@ def model_roi_estimator(input_image_shape, name='roi_estimator'):
     model.summary()
     return model
 
-def embed_roi_images(args):
-    roi_img1, roi_img2, r1, r2, bg_img = args
+def mask_images(args):
+    img, r1, r2 = args
 
     def aux_fn(args):
-        roi_img,r = args
+        img, r = args
         y1 = r[0]
         x1 = r[1]
         y2 = r[2]
@@ -209,42 +210,54 @@ def embed_roi_images(args):
         w = tf.maximum(w, eps)
         y = tf.minimum(y, input_image_size[0] - h)
         x = tf.minimum(x, input_image_size[1] - h)
-        resized_roi_img = tf.image.resize(roi_img, (h,w))
-        padded_roi_img = tf.image.pad_to_bounding_box(resized_roi_img, y, x, input_image_size[0], input_image_size[1])
-        d = 1.0 # dummy
-        mask = (resized_roi_img + d) / (resized_roi_img+ d)
-        fg_mask = tf.image.pad_to_bounding_box(mask, y, x, input_image_size[0], input_image_size[1])
-        return tf.stack([padded_roi_img, fg_mask])
+        ones = tf.ones((h, w, 3))
+        mask = tf.image.pad_to_bounding_box(ones, y, x, input_image_size[0], input_image_size[1])
+        return mask
 
-    res1 = tf.transpose(tf.map_fn(fn=aux_fn, elems=(roi_img1, r1), dtype=tf.float32), [1,0,2,3,4])
-    padded_roi_img1 = res1[0]
-    fg_mask1 = res1[1]
-    res2 = tf.transpose(tf.map_fn(fn=aux_fn, elems=(roi_img2, r2), dtype=tf.float32), [1,0,2,3,4])
-    padded_roi_img2 = res2[0]
-    fg_mask2 = res2[1]
+    mask1 = tf.map_fn(fn=aux_fn, elems=(img, r1), dtype=tf.float32)
+    mask2 = tf.map_fn(fn=aux_fn, elems=(img, r2), dtype=tf.float32)
+    mask = tf.math.maximum(mask1, mask2)
 
-    fg_mask = tf.math.maximum(fg_mask1, fg_mask2)
-    bg_mask = 1.0 - fg_mask
+    mask = tfa.image.gaussian_filter2d(mask, (10,10), sigma=4.0)
 
-    merged_img = fg_mask1 * padded_roi_img1 + fg_mask2 * padded_roi_img2 # + bg_mask * bg_img
-    return merged_img
+    return mask * img
 
 def model_static_attention_estimator(input_image_shape, noise_stddev=0.2, use_color_augmentation=False, use_geometrical_augmentation=True, name='static_attention_estimator'):
     image_input = tf.keras.Input(shape=(input_image_shape))
     input_noise = tf.keras.Input(shape=(2,))
     
     x = image_input
-    x = tf.keras.layers.GaussianNoise(noise_stddev)(x)
 
     if use_color_augmentation:
         x = model.ColorAugmentation()(x)
     if use_geometrical_augmentation:
         x = model.GeometricalAugmentation()(x, input_noise)
-        
-    encoded_img1 = model_roi_ae(input_image_shape, name='roi_encoder1')(x)
-    encoded_img2 = model_roi_ae(input_image_shape, name='roi_encoder2')(x)
-    roi_params = model_roi_estimator(input_image_shape)(image_input)
 
+    x = tf.keras.layers.GaussianNoise(noise_stddev)(x)
+    encoded_img = model_encoder(input_image_shape, name='encoder')(x)
+
+    # x = tf.keras.layers.Flatten()(encoded_img)
+    # x = tf.keras.layers.Dense(128, activation='selu')(x)
+
+    # channels = input_image_shape[2]
+    # nblocks = 4
+    # h = int(input_image_shape[0]/2**nblocks)
+    # w = int(input_image_shape[1]/2**nblocks)
+
+    # x = tf.keras.layers.Dense(h*w*64, activation='selu')(x)
+    # x = tf.keras.layers.BatchNormalization()(x)
+    # x = tf.keras.layers.Reshape(target_shape=(h,w,64))(x)
+    
+    decoded_img = model_decoder(input_image_shape, name='decoder')(encoded_img)
+
+    # MLP -> roi_params
+    x = tf.keras.layers.Flatten()(encoded_img)
+    x = tf.keras.layers.Dense(32, activation='selu')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dense(16, activation='selu')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    roi_params = tf.keras.layers.Dense(6, activation='sigmoid')(x)
+    
     c1 = tf.keras.layers.Lambda(lambda x:x[:,:2], output_shape=(2,))(roi_params)
     s1 = tf.keras.layers.Lambda(lambda x:x[:,2], output_shape=(1,))(roi_params)
     c2 = tf.keras.layers.Lambda(lambda x:x[:,3:5], output_shape=(2,))(roi_params)
@@ -252,13 +265,13 @@ def model_static_attention_estimator(input_image_shape, noise_stddev=0.2, use_co
     r1 = tf.keras.layers.Lambda(roi_rect1)([c1, s1])
     r2 = tf.keras.layers.Lambda(roi_rect1)([c2, s2])
 
-    merged_img = tf.keras.layers.Lambda(embed_roi_images)([encoded_img1, encoded_img2, r1, r2, image_input])
+    masked_img = tf.keras.layers.Lambda(mask_images)([image_input, r1, r2])
 
-    merged_img = model_roi_ae(input_image_shape, name='predictor')(merged_img)
+    predicted_img = model_ae(input_image_shape, name='predictor')(masked_img) # predict the masked part of the image
     
     #m = Model(inputs=[image_input, input_noise], outputs=[encoded_img1, encoded_img2, roi_params], name=name)
     m = StaticAttentionEstimatorModel(inputs=[image_input, input_noise],
-                                          outputs=[merged_img, roi_params, encoded_img1, encoded_img2], name='static_attention_estimator')
+                                          outputs=[predicted_img, decoded_img, masked_img, roi_params], name='static_attention_estimator')
     m.summary()
     return m
 
@@ -274,7 +287,7 @@ def train_sae():
     tr.train(epochs=800, early_stop_patience=800, reduce_lr_patience=100)
     return tr
 
-def prepare_for_test_sae(cp='ae_cp.reaching-real.static_attention_estimator.20220607195210'):
+def prepare_for_test_sae(cp='ae_cp.reaching-real.static_attention_estimator.20220608175756'):
     val_ds = Dataset(dataset, joint_range_data=joint_range_data)
     val_ds.load(groups=val_groups, image_size=input_image_size)
     tr = trainer.Trainer(model_sae, None, val_ds, checkpoint_file=cp)
@@ -284,20 +297,20 @@ def prepare_for_test_sae(cp='ae_cp.reaching-real.static_attention_estimator.2022
 # if __name__ == "__main__":
 #     train_sae()
 
-# val_ds = Dataset(dataset, joint_range_data=joint_range_data)
-# val_ds.load(groups=val_groups, image_size=input_image_size)
-# imgs = np.array(val_ds.data[0][1])
-# roi_imgs1 = imgs[:4]
-# roi_imgs2 = imgs[4:8]
-# bg_imgs = imgs[8:12]
+val_ds = Dataset(dataset, joint_range_data=joint_range_data)
+val_ds.load(groups=val_groups, image_size=input_image_size)
+imgs = np.array(val_ds.data[0][1])
+roi_imgs1 = imgs[:4]
+roi_imgs2 = imgs[4:8]
+bg_imgs = imgs[8:12]
 
-# r1 = np.array([[0.2,0.2,0.8,0.8],
-#                [0.1,0.2,0.7,0.8],
-#                [0.3,0.3,0.5,0.5],
-#                [0.0,0.1,0.4,0.6]])
-# r2 = np.array([[0.5,0.5,0.8,0.8],
-#                [0.4,0.5,0.7,0.8],
-#                [0.2,0.4,0.5,0.5],
-#                [0.6,0.5,0.9,0.8]])
+r1 = np.array([[0.2,0.2,0.7,0.6],
+               [0.1,0.2,0.6,0.7],
+               [0.3,0.3,0.5,0.5],
+               [0.0,0.1,0.4,0.6]])
+r2 = np.array([[0.5,0.5,0.9,0.9],
+               [0.4,0.5,0.8,0.8],
+               [0.2,0.4,0.5,0.5],
+               [0.6,0.5,0.9,0.8]])
 
 #embed_roi_images((roi_img1,roi_img2,r1,r2,bg_img))
