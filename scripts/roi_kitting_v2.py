55eec2e64f02236be7d0ab0a81df985a43192126
Ryo Hanai <ryo@MacbookPro2021.local>
2022-12-09 21:25:07 +0900

    linting

--- scripts/roi_kitting_v2.py
+++ scripts/roi_kitting_v2.py
@@ -16,6 +16,7 @@ from tensorflow import keras
 from tensorflow.keras import layers, losses
 from tensorflow.keras.models import Model
 from AttentionLSTMCell import *
+from mpl_toolkits.mplot3d import Axes3D, axes3d
 
 os.environ['TF_GPU_THREAD_MODE'] = 'gpu_private'
 
@@ -71,16 +72,16 @@ class WeightedFeaturePredictorModel(tf.keras.Model):
                 joint_loss += tf.reduce_mean(tf.square(y_joints_tr[n] - pred_joint))
 
                 if n < self.n_steps:
-                    x_image_tr = tf.transpose(x_image, [1,0,2,3,4])
+                    x_image_tr = tf.transpose(x_image, [1, 0, 2, 3, 4])
                     x_image_tr_list = tf.unstack(x_image_tr)
                     x_image_tr_list[1:].append(pred_image)
                     x_image_tr = tf.stack(x_image_tr_list)
-                    x_image = tf.transpose(x_image_tr, [1,0,2,3,4])
-                    x_joint_tr = tf.transpose(x_joint, [1,0,2])
+                    x_image = tf.transpose(x_image_tr, [1, 0, 2, 3, 4])
+                    x_joint_tr = tf.transpose(x_joint, [1, 0, 2])
                     x_joint_tr_list = tf.unstack(x_joint_tr)
                     x_joint_tr_list[1:].append(pred_joint)
                     x_joint_tr = tf.stack(x_joint_tr_list)
-                    x_joint = tf.transpose(x_joint_tr, [1,0,2])
+                    x_joint = tf.transpose(x_joint_tr, [1, 0, 2])
 
             image_loss /= self.n_steps
             joint_loss /= self.n_steps
@@ -105,27 +106,27 @@ class WeightedFeaturePredictorModel(tf.keras.Model):
         joint_loss = 0.0
         y_images, y_joints = y
 
-        y_images_tr = tf.transpose(y_images, [1,0,2,3,4])
-        y_joints_tr = tf.transpose(y_joints, [1,0,2])
+        y_images_tr = tf.transpose(y_images, [1, 0, 2, 3, 4])
+        y_joints_tr = tf.transpose(y_joints, [1, 0, 2])
 
         for n in range(self.n_steps):
-            pred_image, pred_joint, attention_map = self((x_image, x_joint, input_noise), training=False) # Forward pass
+            pred_image, pred_joint, attention_map = self((x_image, x_joint, input_noise), training=False)  # Forward pass
 
             y_image_aug = translate_image(y_images_tr[n], input_noise)
             image_loss += tf.reduce_mean(tf.square(y_image_aug - pred_image))
             joint_loss += tf.reduce_mean(tf.square(y_joints_tr[n] - pred_joint))
 
             if n < self.n_steps:
-                x_image_tr = tf.transpose(x_image, [1,0,2,3,4])
+                x_image_tr = tf.transpose(x_image, [1, 0, 2, 3, 4])
                 x_image_tr_list = tf.unstack(x_image_tr)
                 x_image_tr_list[1:].append(pred_image)
                 x_image_tr = tf.stack(x_image_tr_list)
-                x_image = tf.transpose(x_image_tr, [1,0,2,3,4])
-                x_joint_tr = tf.transpose(x_joint, [1,0,2])
+                x_image = tf.transpose(x_image_tr, [1, 0, 2, 3, 4])
+                x_joint_tr = tf.transpose(x_joint, [1, 0, 2])
                 x_joint_tr_list = tf.unstack(x_joint_tr)
                 x_joint_tr_list[1:].append(pred_joint)
                 x_joint_tr = tf.stack(x_joint_tr_list)
-                x_joint = tf.transpose(x_joint_tr, [1,0,2])
+                x_joint = tf.transpose(x_joint_tr, [1, 0, 2])
 
         image_loss /= self.n_steps
         joint_loss /= self.n_steps
@@ -136,33 +137,11 @@ class WeightedFeaturePredictorModel(tf.keras.Model):
         self.test_trackers['loss'].update_state(loss)
         return dict([(trckr[0], trckr[1].result()) for trckr in self.test_trackers.items()])
 
-    # def compute_loss(self, y, y_pred, input_noise):
-    #     y_image, y_joint = y
-    #     # image_aug = translate_image(y_image, input_noise)
-    #     y_image_aug = translate_image(y_image[:,0], input_noise[:,0])
-
-    #     image_loss = tf.reduce_mean(tf.square(y_image_aug - y_pred[0]))
-    #     # joint_loss = tf.reduce_mean(tf.square(y_joint - y_pred[1]))
-    #     joint_loss = tf.reduce_mean(tf.square(y_joint[:,0] - y_pred[1]))
-    #     loss = image_loss + joint_loss
-    #     return image_loss, joint_loss, loss
-
-    # def compute_loss2(self, y, y_pred, input_noise):
-    #     y_images, y_joints = y
-    #     y_pred_images, y_pred_joints = y_pred
-
-    #      #image_aug = translate_image(y_image, input_noise)
-
-    #     image_loss = tf.reduce_mean(tf.square(y_images - y_pred_images)) / self.n_steps
-    #     joint_loss = tf.reduce_mean(tf.square(y_joints - y_pred_joints)) / self.n_steps
-    #     loss = image_loss + joint_loss
-    #     return image_loss, joint_loss, loss
-
     @property
     def metrics(self):
         return list(self.train_trackers.values()) + list(self.test_trackers.values())
 
-    
+
 def model_time_distributed_encoder(input_shape, time_window_size, name='encoder'):
     image_input = tf.keras.Input(shape=((time_window_size,) + input_shape))
 
@@ -175,25 +154,6 @@ def model_time_distributed_encoder(input_shape, time_window_size, name='encoder'
     encoder.summary()
     return encoder
 
-def model_lstm(time_window_size, image_vec_dim, dof, use_stacked_lstm=True, name='lstm'):
-    imgvec_input = tf.keras.Input(shape=(time_window_size, image_vec_dim))
-    joint_input = tf.keras.Input(shape=(time_window_size, dof))
-    state_dim = image_vec_dim + dof
-    x = tf.keras.layers.concatenate([imgvec_input, joint_input])
-
-    if use_stacked_lstm:
-        x = tf.keras.layers.LSTM(state_dim, return_sequences=True)(x)
-        x = tf.keras.layers.LSTM(state_dim, return_sequences=True)(x)
-
-    x = tf.keras.layers.LSTM(state_dim)(x)
-    #x = tf.keras.layers.Dense(state_dim)(x)
-
-    predicted_ivec = tf.keras.layers.Lambda(lambda x:x[:,:image_vec_dim], output_shape=(image_vec_dim,))(x)
-    predicted_jvec = tf.keras.layers.Lambda(lambda x:x[:,image_vec_dim:image_vec_dim+dof], output_shape=(dof,))(x)
-
-    m = keras.Model([imgvec_input, joint_input], [predicted_ivec, predicted_jvec], name=name)
-    m.summary()
-    return m
 
 def model_decoder(input_shape, name='decoder'):
     input_feature = tf.keras.Input(shape=(input_shape))
@@ -201,8 +161,8 @@ def model_decoder(input_shape, name='decoder'):
     x = deconv_block(x, 32)
     x = deconv_block(x, 16)
     decoded_img = tf.keras.layers.Conv2DTranspose(3, kernel_size=3, strides=1, padding='same', activation='sigmoid')(x)
-    
-    decoder = keras.Model([input_feature], decoded_img, name=name)    
+
+    decoder = keras.Model([input_feature], decoded_img, name=name)
     decoder.summary()
     return decoder
 
@@ -211,7 +171,7 @@ def model_weighted_feature_prediction(input_image_shape, time_window_size, image
     image_input = tf.keras.Input(shape=((time_window_size,) + input_image_shape))
     joint_input = tf.keras.Input(shape=(time_window_size, dof))
     noise_input = tf.keras.Input(shape=(2,))
-    
+
     x = image_input
 
     # This should be changed to time-series version
@@ -219,11 +179,11 @@ def model_weighted_feature_prediction(input_image_shape, time_window_size, image
         x = TimeDistributedColorAugmentation()(x)
     x = TimeDistributedGeometricalAugmentation()(x, noise_input)
     x = tf.keras.layers.GaussianNoise(image_noise)(x)
-    
+
     # convert to feature map
     image_feature = model_time_distributed_encoder(input_image_shape, time_window_size, name='feature-encoder')(x)
 
-    #joint_input_with_noise = tf.keras.kayers.TimeDistributed(tf.keras.layers.GaussianNoise(joint_noise))(joint_input)
+    # joint_input_with_noise = tf.keras.kayers.TimeDistributed(tf.keras.layers.GaussianNoise(joint_noise))(joint_input)
     joint_input_with_noise = tf.keras.layers.GaussianNoise(joint_noise)(joint_input)
 
     cell = AttentionLSTMCell(image_vec_dim + dof)
@@ -231,8 +191,8 @@ def model_weighted_feature_prediction(input_image_shape, time_window_size, image
     x, attention_map = layer((image_feature, joint_input_with_noise))
 
     # split output vector (293, 7)
-    predicted_ivec = tf.keras.layers.Lambda(lambda x:x[:,:image_vec_dim], output_shape=(image_vec_dim,))(x)
-    predicted_jvec = tf.keras.layers.Lambda(lambda x:x[:,image_vec_dim:], output_shape=(dof,))(x)
+    predicted_ivec = tf.keras.layers.Lambda(lambda x: x[:, :image_vec_dim], output_shape=(image_vec_dim,))(x)
+    predicted_jvec = tf.keras.layers.Lambda(lambda x: x[:, image_vec_dim:], output_shape=(dof,))(x)
 
     channels = 64
     h = 5
@@ -243,7 +203,7 @@ def model_weighted_feature_prediction(input_image_shape, time_window_size, image
 
     x = deconv_block(x, 128)
     x = deconv_block(x, channels)
-    
+
     # decode to the next frame
     predicted_img = model_decoder((h*4, w*4, channels))(x)
 
@@ -253,7 +213,7 @@ def model_weighted_feature_prediction(input_image_shape, time_window_size, image
 
     m.summary()
     return m
-    
+
 
 wf_predictor = model_weighted_feature_prediction(input_image_size+(3,), time_window_size, latent_dim, dof)
 
@@ -269,52 +229,49 @@ def train():
     tr.train(epochs=800, early_stop_patience=800, reduce_lr_patience=100)
 
 
-from mpl_toolkits.mplot3d import Axes3D, axes3d
-
-
 class Predictor(trainer.TimeSequenceTrainer):
-    def __init__(self, *args, time_window_size=20,**kargs):
+    def __init__(self, *args, time_window_size=20, **kargs):
         super(Predictor, self).__init__(*args, time_window_size=time_window_size, **kargs)
 
     def get_data(self):
         return next(self.val_gen)
 
     def get_a_sample_from_batch(self, batch, n):
-        x,y = batch
+        x, y = batch
         x_img = x[0][n:n+1]
         x_joint = x[1][n:n+1]
         y_img = y[0][n:n+1]
         y_joint = y[1][n:n+1]
-        return (x_img,x_joint),(y_img,y_joint)
+        return (x_img, x_joint), (y_img, y_joint)
 
-    ## evaluation of predictor
+    # evaluation of predictor
     def predict_with_roi(self, batch, roi_params):
-        x,y = batch
+        x, y = batch
         batch_sz = x[1].shape[0]
         if roi_params.ndim == 1:
-            roi_params = np.tile(roi_params, (batch_sz,1))
+            roi_params = np.tile(roi_params, (batch_sz, 1))
         predicted_images, predicted_joints = self.model.predict(x + (roi_params,))
-        #visualize_ds(y[0], roi_params) # to draw ROI roi_params must be converted to rectangle
+        # visualize_ds(y[0], roi_params) # to draw ROI roi_params must be converted to rectangle
 
         if y[0].ndim == 5:
-            bboxes = roi_rect1((roi_params[:,:2], roi_params[:,2]))
-            imgs = draw_bounding_boxes(x[0][:,-1], bboxes)
+            bboxes = roi_rect1((roi_params[:, :2], roi_params[:, 2]))
+            imgs = draw_bounding_boxes(x[0][:, -1], bboxes)
             visualize_ds(imgs)
         else:
             visualize_ds(y[0])
         visualize_ds(predicted_images)
         plt.show()
 
-    def generate_roi_images(self, sample, n = 5):
+    def generate_roi_images(self, sample, n=5):
         xs = np.linspace(0.1, 0.9, n)
         ys = np.linspace(0.1, 0.9, n)
         ss = 0.7 * np.sin(np.pi*xs)
         out_images = []
-        for roi_params in zip(xs,ys,ss):
+        for roi_params in zip(xs, ys, ss):
             roi_params = np.expand_dims(np.array(roi_params), 0)
             predicted_images, _ = self.model.predict(sample[0] + (roi_params,))
             imgs = sample[0][0][0][-1:]
-            bboxes = roi_rect1((roi_params[:,:2], roi_params[:,2]))
+            bboxes = roi_rect1((roi_params[:, :2], roi_params[:, 2]))
             img_with_bb = draw_bounding_boxes(imgs, bboxes)[0]
             img = np.concatenate([img_with_bb, predicted_images[0]], axis=1)
             out_images.append(img)
@@ -322,8 +279,8 @@ class Predictor(trainer.TimeSequenceTrainer):
             # plt.show()
         create_anim_gif_from_images(out_images, 'generated_roi_images.gif')
 
-    def prediction_error(self, sample, roi_pos=[0.5,0.5], roi_scale=0.8):
-        x,y = sample
+    def prediction_error(self, sample, roi_pos=[0.5, 0.5], roi_scale=0.8):
+        x, y = sample
         batch_size = tf.shape(x[0])[0]
         roi_pos = tf.tile([roi_pos], (batch_size, 1))
         roi_scale = tf.repeat(roi_scale, batch_size)
@@ -334,8 +291,8 @@ class Predictor(trainer.TimeSequenceTrainer):
         x = np.linspace(0.0, 1.0, nx, dtype='float32')
         y = np.linspace(0.0, 1.0, ny, dtype='float32')
         s = np.linspace(smin, smax, ns, dtype='float32')
-        x,y,s = np.meshgrid(x, y, s)
-        z = np.array([self.prediction_error(sample, [x,y], s)[1] for x,y,s in zip(x.flatten(), y.flatten(), s.flatten())]).reshape((nx,ny,ns))
+        x, y, s = np.meshgrid(x, y, s)
+        z = np.array([self.prediction_error(sample, [x, y], s)[1] for x, y, s in zip(x.flatten(), y.flatten(), s.flatten())]).reshape((nx, ny, ns))
         idx = np.unravel_index(np.argmin(z), z.shape)
         xopt = x[idx]
         yopt = y[idx]
@@ -351,25 +308,25 @@ class Predictor(trainer.TimeSequenceTrainer):
         ax.set_xlabel('v')
         ax.set_ylabel('u')
         ax.zaxis.set_major_formatter(ptick.ScalarFormatter(useMathText=True))
-        ax.ticklabel_format(style='sci', axis='z', scilimits=(0,0))
-        ax.plot_surface(x[:,:,sopt_idx], y[:,:,sopt_idx], z[:,:,sopt_idx], cmap='plasma')
+        ax.ticklabel_format(style='sci', axis='z', scilimits=(0, 0))
+        ax.plot_surface(x[:, :, sopt_idx], y[:, :, sopt_idx], z[:, :, sopt_idx], cmap='plasma')
 
         plt.show()
         # return x,y,z,imgs[0]
 
     # evaluation of ROI estimator
     def predict_images(self, batch=None):
-        if batch == None:
-            x,y = next(self.val_gen)
+        if batch is None:
+            x, y = next(self.val_gen)
         else:
-            x,y = batch
+            x, y = batch
         batch_size = tf.shape(x[0])[0]
 
-        roi_params = np.tile([0.5,0.5,0.8], (batch_size,1))
+        roi_params = np.tile([0.5, 0.5, 0.8], (batch_size, 1))
         roi_params = self.model.predict(x+(roi_params,))
-        bboxes = roi_rect1((roi_params[:,:2], roi_params[:,2]))
-        
-        imgs = draw_bounding_boxes(x[0][:,-1], bboxes)
+        bboxes = roi_rect1((roi_params[:, :2], roi_params[:, 2]))
+
+        imgs = draw_bounding_boxes(x[0][:, -1], bboxes)
         visualize_ds(imgs)
         # predicted_images, predicted_joints = self.model.predictor.predict(x + (roi_params,))
         # visualize_ds(predicted_images)
@@ -387,18 +344,13 @@ class Predictor(trainer.TimeSequenceTrainer):
     def predict(self, x):
         'only used for Predictor test'
         batch_sz = x[1].shape[0]
-        roi_params0 = np.tile([0.5,0.5,0.8], (batch_sz,1))
+        roi_params0 = np.tile([0.5, 0.5, 0.8], (batch_sz, 1))
         pred_img, pred_joint = self.model.predict(x+(roi_params0,))
-        bboxes = roi_rect1((roi_params0[:,:2], roi_params0[:,2]))
+        bboxes = roi_rect1((roi_params0[:, :2], roi_params0[:, 2]))
         return pred_img, pred_joint, bboxes
 
     def get_validation_sample(self, group_num=0, start_point=0):
-        res = []
         d = self.val_ds.data
-        seq_len = len(d[group_num][1])
-        ishape = d[group_num][1][0].shape
-        jv_dim = d[group_num][0].shape[1]
-
         batch_x_jv = np.expand_dims(d[group_num][0][start_point:start_point+self.time_window], axis=0)
         batch_y_jv = np.expand_dims(d[group_num][0][start_point+self.time_window:start_point+self.time_window+self.val_gen.prediction_window_size], axis=0)
         batch_x_img = np.expand_dims(np.array(d[group_num][1][start_point:start_point+self.time_window]), axis=0)
@@ -419,7 +371,7 @@ class Predictor(trainer.TimeSequenceTrainer):
 
         roi_params = np.tile([0.5,0.5,0.8], (batch_size,1))
         results = []
-        
+
         for seq_num in range(seq_len-self.time_window):
             print(seq_num)
             batch_x_jvs[:] = d[group_num][0][seq_num:seq_num+self.time_window]
@@ -436,12 +388,13 @@ class Predictor(trainer.TimeSequenceTrainer):
 
 class Tester(trainer.Trainer):
 
-    def __init__(self, model,
-                     val_dataset,
-                     time_window_size=time_window_size,
-                     batch_size=32,
-                     runs_directory=None,
-                     checkpoint_file=None):
+    def __init__(self, 
+                 model,
+                 val_dataset,
+                 time_window_size=time_window_size,
+                 batch_size=32,
+                 runs_directory=None,
+                 checkpoint_file=None):
 
         super(Tester, self).__init__(model,
                                      None,
@@ -454,17 +407,15 @@ class Tester(trainer.Trainer):
 
         if val_dataset:
             self.val_gen = generator.DPLGenerator().flow(val_dataset.get(),
-                                                             None,
-                                                             batch_size=self.batch_size,
-                                                             time_window_size=self.time_window,
-                                                             prediction_window_size=5,
-                                                             add_roi=False)
+                                                         None,
+                                                         batch_size=self.batch_size,
+                                                         time_window_size=self.time_window,
+                                                         prediction_window_size=5,
+                                                         add_roi=False)
             self.val_data_loaded = True
 
-
     def predict_images(self, random_shift=True, return_data=False):
-        x,y = next(self.val_gen)
-        rois = []
+        x, y = next(self.val_gen)
 
         if random_shift:
             noise = tf.zeros(shape=(x[0].shape[0], 2))
@@ -477,7 +428,8 @@ class Tester(trainer.Trainer):
         visualize_ds(x[0][:,-1,:,:,:])
         visualize_ds(predicted_images)
 
-        a = attention_map[:,-1]
+        # a = attention_map[:,-1]
+        a = attention_map
         a /= (np.max(a) - np.min(a))
         visualize_ds(a)
         plt.show()
@@ -497,7 +449,7 @@ class Tester(trainer.Trainer):
         batch_y_jv = np.empty((batch_size, jv_dim))
 
         results = []
-        
+
         for seq_num in range(seq_len-self.time_window):
             print(seq_num)
             batch_x_jvs[:] = d[group_num][0][seq_num:seq_num+self.time_window]
@@ -512,18 +464,19 @@ class Tester(trainer.Trainer):
                 y_pred = self.model.predict((batch_x_imgs, batch_x_jvs))
 
             predicted_images, predicted_joints, attention_maps = y_pred
-            b,rect = attention_map2roi.apply_filters(batch_x_imgs[:,-1], attention_maps[:,-1], visualize_result=False, return_result=True)
+            # attention_maps = attention_maps[:, -1]
+            b, rect = attention_map2roi.apply_filters(batch_x_imgs[:, -1], attention_maps, visualize_result=False, return_result=True)
             if out_roi_image:
-                roi_imgs = tf.image.crop_and_resize(batch_x_imgs[:,-1], tf.cast(rect, tf.float32), range(batch_size), input_image_size)
+                roi_imgs = tf.image.crop_and_resize(batch_x_imgs[:, -1], tf.cast(rect, tf.float32), range(batch_size), input_image_size)
                 res = tf.concat([b[0], roi_imgs[0]], axis=0)
             else:
-                resized_attention_map = tf.image.grayscale_to_rgb(tf.image.resize(attention_maps[0,-1], b[0].shape[:2]))
+                resized_attention_map = tf.image.grayscale_to_rgb(tf.image.resize(attention_maps[0], b[0].shape[:2]))
                 res = tf.concat([b[0], resized_attention_map], axis=0)
             results.append(res.numpy())
 
         create_anim_gif_from_images(results, out_filename='estimated_roi_g{:05d}.gif'.format(group_num))
 
-        
+
 def prepare_for_test(cp='ae_cp.kitting.weighted_feature_prediction.20220907161250'):
     # ae_cp.reaching-real.weighted_feature_prediction.20220623184031
     val_ds = Dataset(dataset, joint_range_data=joint_range_data)
