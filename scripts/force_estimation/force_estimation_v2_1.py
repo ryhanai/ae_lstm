a4fb41df1bd265c94d24b153cb2a9037af8f98a2
Ryo Hanai <ryo@MacbookPro2021.local>
2022-12-27 10:31:04 +0900

    refactoring force_estimation

--- scripts/force_estimation/force_estimation_v2_1.py
+++ scripts/force_estimation/force_estimation_v2_1.py
@@ -1,40 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from typing import Concatenate
-import os, time
-from datetime import datetime
-from pybullet_tools import *
-# import force_distribution_viewer
-import matplotlib.pyplot as plt
 import numpy as np
-import scipy.linalg
-import res_unet
 import tensorflow as tf
 import tensorflow_addons as tfa
-from core.utils import *
-from force_estimation_data_loader import ForceEstimationDataLoader
-import forcemap
 from model import *
-from tensorflow import keras
-from tensorflow.keras import layers
+import res_unet
 from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.models import Model
 
-dataset = 'basket-filling2'
+
 image_height = 360
 image_width = 512
 input_image_shape = [image_height, image_width]
 num_classes = 62
-
-dl = ForceEstimationDataLoader(os.path.join(os.environ['HOME'], 'Dataset/dataset2', dataset),
-                               os.path.join(os.environ['HOME'], 'Dataset/dataset2', dataset+'-real'),
-                               image_height=image_height,
-                               image_width=image_width,
-                               start_seq=1,
-                               n_seqs=1800,  # n_seqs=1500,
-                               start_frame=3, n_frames=3,
-                               real_start_frame=1, real_n_frames=294
-                               )
 
 
 def augment(xs, ys):
@@ -221,7 +199,7 @@ def model_simple_decoder(input_shape, name='decoder'):
     x = deconv_block(x, 32, with_upsampling=False)    # 48x64
     x = tf.keras.layers.Conv2DTranspose(20, kernel_size=3, strides=1, padding='same', activation='sigmoid')(x)
     x = tf.keras.layers.Resizing(40, 40)(x)
-    
+
     decoder_output = x
     decoder = keras.Model(feature_input, decoder_output, name=name)
     decoder.summary()
@@ -240,7 +218,7 @@ def model_resnet_decoder(input_shape, name='resnet_decoder'):
     x = tf.keras.layers.Conv2DTranspose(20, kernel_size=3, strides=1, padding='same', activation='sigmoid')(x)
     x = tf.keras.layers.Resizing(40, 40)(x)
     decoder_output = x
-    decoder = keras.Model(feature_input, decoder_output, name=name)
+    decoder = tf.keras.Model(feature_input, decoder_output, name=name)
     decoder.summary()
     return decoder
 
@@ -301,277 +279,3 @@ def model_depth_to_fmap(input_shape=input_image_shape, kernel_size=3, num_classe
     model.summary()
 
     return model
-
-
-class Trainer:
-    def __init__(self, model,
-                 train_dataset,
-                 val_dataset,
-                 # load_weight=False,
-                 batch_size=32,
-                 runs_directory=None,
-                 checkpoint_file=None):
-
-        self.input_image_shape = val_dataset[0][0].shape
-        self.batch_size = batch_size
-
-        self.model = model
-        self.opt = keras.optimizers.Adamax(learning_rate=0.001)
-        self.model.compile(loss='mse', optimizer=self.opt)
-
-        if train_dataset:
-            self.train_ds = train_dataset
-
-        if val_dataset:
-            self.val_ds = val_dataset
-            self.val_data_loaded = True
-
-        d = runs_directory if runs_directory else os.path.join(os.path.dirname(os.getcwd()), 'runs')
-        f = checkpoint_file if checkpoint_file else 'ae_cp.{}.{}.{}'.format(dataset, self.model.name, datetime.now().strftime('%Y%m%d%H%M%S'))
-        self.checkpoint_path = os.path.join(d, f, 'cp.ckpt')
-
-        if checkpoint_file:
-            print('load weights from ', checkpoint_file)
-            self.model.load_weights(self.checkpoint_path)
-
-    def prepare_callbacks(self, early_stop_patience=100, reduce_lr_patience=50):
-        # Create a callback that saves the model's weights
-        cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=self.checkpoint_path,
-                                                         save_weights_only=True,
-                                                         verbose=1,
-                                                         mode='min',
-                                                         save_best_only=True)
-
-        # early stopping if not changing for 50 epochs
-        early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
-                                                      patience=early_stop_patience)
-
-        # reduce learning rate
-        reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss',
-                                                         factor=0.1,
-                                                         patience=reduce_lr_patience,
-                                                         verbose=1,
-                                                         min_lr=0.00001)
-
-        profiler = tf.keras.callbacks.TensorBoard(log_dir='logs',
-                                                  histogram_freq=1,
-                                                  profile_batch='15,25')
-        return cp_callback, early_stop, reduce_lr, profiler
-
-    def train(self, epochs=300, early_stop_patience=100, reduce_lr_patience=50):
-        xs = self.train_ds[0].astype('float32')
-        ys = self.train_ds[1].astype('float32')
-        val_xs = self.val_ds[0].astype('float32')
-        val_ys = self.val_ds[1].astype('float32')
-
-        start = time.time()
-        callbacks = self.prepare_callbacks(early_stop_patience, reduce_lr_patience)
-
-        source_dataset = tf.data.Dataset.from_tensor_slices((xs, ys)).shuffle(len(xs)).batch(self.batch_size)
-        # target_dataset = tf.data.Dataset.from_tensor_slices(xs_real).shuffle(len(x_real)).repeat().batch(self.batch_size)
-        # train_dataset = tf.data.Dataset.zip((source_dataset, target_dataset))
-
-        val_source_dataset = tf.data.Dataset.from_tensor_slices((val_xs, val_ys)).batch(self.batch_size)
-        # # val_target_dataset = tf.data.Dataset.from_tensor_slices(val_xs).shuffle(BUFFER_SIZE).repeat().batch(self.batch_size)
-        # val_dataset = tf.data.Dataset.zip((val_source_dataset, val_target_dataset))
-
-        history = self.model.fit(source_dataset,
-                                 batch_size=self.batch_size,
-                                 epochs=epochs,
-                                 callbacks=callbacks,
-                                 validation_data=val_source_dataset,
-                                 shuffle=True)
-
-        end = time.time()
-        print('\ntotal time spent for training: {}[min]'.format((end-start)/60))
-
-
-def visualize_result(f_prediction, f_label, rgb, filename=None):
-
-    plt.savefig('prediction.png')
-    p = plt.imread('prediction.png')[:, :, :3]
-    if f_label is None:
-        p = cv2.resize(p, (1440,360))
-        res = np.concatenate([rgb, p], axis=1)
-    if f_label is not None:
-        forcemap.plot_force_map(f_label, 'ground truth')
-        plt.savefig('ground_truth.png')
-        g = plt.imread('ground_truth.png')[:, :, :3]
-        pg = np.concatenate([p, g], axis=0)
-        rgb2 = np.ones((800, 512, 3))
-        rgb2[220:580] = rgb
-        res = np.concatenate([rgb2, pg], axis=1)
-    fig = plt.figure(figsize=(10, 4))
-    ax = fig.add_subplot(111)
-    ax.axis('off')
-    plt.imshow(res)
-    if filename is not None:
-        plt.savefig(filename)
-    plt.show()
-
-
-class Tester:
-    def __init__(self, model, test_data, checkpoint_file, runs_directory=None):
-        self.model = model
-        d = runs_directory if runs_directory else os.path.join(os.path.dirname(os.getcwd()), 'runs')
-        self.checkpoint_path = os.path.join(d, checkpoint_file, 'cp.ckpt')
-        print('load weights from ', self.checkpoint_path)
-        self.model.load_weights(self.checkpoint_path)
-        self.test_data = test_data
-
-    def predict(self, n):
-        xs = self.test_data[0][n:n+1]
-        # ys = self.test_data[1][n:n+1]
-        y_pred = self.model.predict(xs)
-        plt.imshow(xs[0])
-        forcemap.plot_force_map(y_pred[0], 'prediction')
-        plt.show()
-
-    def predict_segmentation_mask(self, n):
-        xs = self.test_data[0][n:n+1]
-        ys = self.test_data[1][n:n+1]
-        y_pred_logits = self.model.predict(xs)
-        plt.figure()
-        plt.imshow(xs[0])
-        y_pred = tf.cast(tf.argmax(y_pred_logits, axis=-1), dtype=tf.uint8)
-        plt.figure()
-        plt.imshow(y_pred[0])
-        plt.figure()
-        plt.imshow(ys[0])
-        plt.show()
-        return xs[0], ys[0], y_pred[0]
-
-    def predict_force_from_rgb(self, n, visualize=True):
-        if type(self.test_data) is tuple:
-            xs = self.test_data[0][n:n+1]
-            force_label = self.test_data[1][n]
-        else:
-            xs = self.test_data[n:n+1]
-            force_label = None
-        y_preds = self.model.predict(xs)
-        y_pred_forces = y_preds
-        visualize_result(y_pred_forces[0], force_label, xs[0], 'result{:05d}.png'.format(n))
-        return y_pred_forces[0], force_label, xs[0]
-
-    def predict_force_from_rgb_with_img(self, rgb):
-        xs = np.expand_dims(rgb, axis=0)
-        y_preds = self.model.predict(xs)
-        y_pred_forces = y_preds
-        forcemap.plot_force_map(y_pred_forces[0], 'prediction')
-        plt.show()
-        return y_pred_forces
-
-    def predict_force_from_depth_seg(self, n, rgb):
-        xs_depth = self.test_data[0][0][n:n+1]
-        xs_seg = self.test_data[0][1][n:n+1]
-        xs = xs_depth, xs_seg
-        y_preds = self.model.predict(xs)
-        y_pred_forces = y_preds
-        force_label = self.test_data[1][n]
-
-        visualize_result(y_pred_forces[0], force_label, rgb[n], 'result{:05d}.png'.format(n))
-        return y_pred_forces[0], force_label, rgb[n]
-
-
-def show_bin_state(fcam, bin_state, fmap, draw_fmap=True, draw_force_gradient=False):
-    fv = np.zeros((40, 40, 40))
-    fv[:, :, :20] = fmap
-    positions = fcam.positions
-    viewer.publish_bin_state(bin_state, positions, fv, draw_fmap=draw_fmap, draw_force_gradient=draw_force_gradient)
-
-
-def pick_direction_plan(fcam, n=25, gp=[0.02, -0.04, 0.79], radius=0.05, scale=[0.005, 0.01, 0.004]):
-    fmap, force_label, rgb = tester.predict_force_from_rgb(n, visualize_result=False)
-    bin_state = tester.test_data[2][n]
-
-    gp = np.array(gp)
-    fv = np.zeros((40, 40, 40))
-    fv[:, :, :20] = fmap
-    gxyz = np.gradient(-fv)
-    g_vecs = np.column_stack([g.flatten() for g in gxyz])
-
-    ps = fcam.positions
-    idx = np.where(np.sum((ps - gp)*g_vecs, axis=1) < 0)[0]
-    fps = ps[idx]
-    fg_vecs = g_vecs[idx]
-
-    # points for visualization
-    filtered_pos_val_pairs = [(p, g) for (p, g) in zip(fps, fg_vecs) if scipy.linalg.norm(g) > 0.008]
-    pz, vz = zip(*filtered_pos_val_pairs)
-    pz = np.array(pz)
-    vz = np.array(vz)
-    viewer.publish_bin_state(bin_state, ps, fv, draw_fmap=False, draw_force_gradient=False)
-    viewer.draw_vector_field(pz, vz, scale=0.3)
-    viewer.rviz_client.draw_sphere(gp, [1, 0, 0, 1], [0.01, 0.01, 0.01])
-    viewer.rviz_client.show()
-
-    # points for planning
-    pz = fps
-    vz = fg_vecs
-    idx = np.where(scipy.linalg.norm(pz - gp, axis=1) < radius)[0]
-    pz = pz[idx]
-    vz = vz[idx]
-    pick_direction = np.sum(vz, axis=0)
-    pick_direction /= np.linalg.norm(pick_direction)
-    viewer.rviz_client.draw_arrow(gp, gp + pick_direction * 0.1, [0, 1, 0, 1], scale)
-    pick_moment = np.sum(np.cross(pz - gp, vz), axis=0)
-    pick_moment /= np.linalg.norm(pick_moment)
-    viewer.rviz_client.draw_arrow(gp, gp + pick_moment * 0.1, [1, 1, 0, 1], scale)
-
-    viewer.rviz_client.show()
-    return pz, vz, pick_direction, pick_moment
-
-
-def virtual_pick(bin_state0, pick_vector, pick_moment, object_name='011_banana', alpha=0.01, beta=0.05, repeat=5):
-    def do_virtual_pick():
-        bin_state = bin_state0
-        for i in range(10):
-            p, q = [s for s in bin_state if s[0] == object_name][0][1]
-            dq = quat_from_euler(beta * pick_moment)
-            dp = alpha * pick_vector
-            p2, q2 = multiply_transforms(dp, dq, p, q)
-            p2 = p + dp
-            bin_state = [(object_name, (p2, q2)) if s[0] == object_name else s for s in bin_state]
-            viewer.publish_bin_state(bin_state, [], [], draw_fmap=False, draw_force_gradient=False)
-            time.sleep(0.2)
-
-    for i in range(repeat):
-        do_virtual_pick()
-
-
-task = 'None'
-# model_file = 'ae_cp.basket-filling.model_resnet.20221115193122' # current best
-# model_file = 'ae_cp.basket-filling.model_resnet.20221125134301'
-model_file = 'ae_cp.basket-filling2.model_resnet.20221202165608'
-
-if task == 'train':
-    model = model_rgb_to_fmap_res50()
-    train_data, valid_data = dl.load_data_for_rgb2fmap(train_mode=True)
-    trainer = Trainer(model, train_data, valid_data)
-elif task == 'adaptation':
-    model = model_rgb_to_fmap_res50()
-    train_data, valid_data = dl.load_data_for_rgb2fmap_with_real(train_mode=True)
-    trainer = Trainer(model, train_data, valid_data)
-elif task == 'test':
-    model = model_rgb_to_fmap_res50()
-    test_data = dl.load_data_for_rgb2fmap(test_mode=True)
-    tester = Tester(model, test_data, model_file)
-elif task == 'test-real':
-    model = model_rgb_to_fmap_res50()
-    test_data = dl.load_real_data_for_rgb2fmap(test_mode=True)
-    tester = Tester(model, test_data, model_file)
-
-
-# tester = Tester(model, test_data, 'ae_cp.basket-filling.force_estimator.best')
-# tester = Tester(model, test_data, 'ae_cp.basket-filling.force_estimator.20221028172410')
-# tester = Tester(model, test_data, 'ae_cp.basket-filling.model_resnet.20221101213121')
-# tester = Tester(model, test_data, 'ae_cp.basket-filling.model_depth_to_fmap.20221104234049')
-
-# best
-# tester = Tester(model, test_data, 'ae_cp.basket-filling.model_depth_to_fmap.20221107110203')
-# tester = Tester(model, test_data, 'ae_cp.basket-filling.model_resnet.20221107144923')
-# tester = Tester(model, test_data, 'ae_cp.basket-filling.model_resnet.20221115154455')
-# tester = Tester(model, test_data, 'ae_cp.basket-filling.model_resnet.20221115182656')
-
-# gaussian / color augmentation
-# tester = Tester(model, test_data, 'ae_cp.basket-filling.model_resnet.20221108181626')
---
+++ scripts/force_estimation/run.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+
+import os
+import time
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+# from core.utils import *
+# from pybullet_tools import *
+import forcemap
+from force_estimation_data_loader import ForceEstimationDataLoader
+import force_estimation_v2_1 as fe
+
+
+dataset = 'basket-filling2'
+image_height = 360
+image_width = 512
+input_image_shape = [image_height, image_width]
+num_classes = 62
+
+fmap = forcemap.GridForceMap('seria_basket')
+
+dl = ForceEstimationDataLoader(os.path.join(os.environ['HOME'], 'Dataset/dataset2', dataset),
+                               os.path.join(os.environ['HOME'], 'Dataset/dataset2', dataset+'-real'),
+                               image_height=image_height,
+                               image_width=image_width,
+                               start_seq=1,
+                               n_seqs=1800,  # n_seqs=1500,
+                               start_frame=3, n_frames=3,
+                               real_start_frame=1, real_n_frames=294
+                               )
+
+
+class Trainer:
+    def __init__(self, model,
+                 train_dataset,
+                 val_dataset,
+                 # load_weight=False,
+                 batch_size=32,
+                 runs_directory=None,
+                 checkpoint_file=None):
+
+        self.input_image_shape = val_dataset[0][0].shape
+        self.batch_size = batch_size
+
+        self.model = model
+        self.opt = tf.keras.optimizers.Adamax(learning_rate=0.001)
+        self.model.compile(loss='mse', optimizer=self.opt)
+
+        if train_dataset:
+            self.train_ds = train_dataset
+
+        if val_dataset:
+            self.val_ds = val_dataset
+            self.val_data_loaded = True
+
+        d = runs_directory if runs_directory else os.path.join(os.path.dirname(os.getcwd()), 'runs')
+        f = checkpoint_file if checkpoint_file else 'ae_cp.{}.{}.{}'.format(dataset, self.model.name, datetime.now().strftime('%Y%m%d%H%M%S'))
+        self.checkpoint_path = os.path.join(d, f, 'cp.ckpt')
+
+        if checkpoint_file:
+            print('load weights from ', checkpoint_file)
+            self.model.load_weights(self.checkpoint_path)
+
+    def prepare_callbacks(self, early_stop_patience=100, reduce_lr_patience=50):
+        # Create a callback that saves the model's weights
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=self.checkpoint_path,
+                                                         save_weights_only=True,
+                                                         verbose=1,
+                                                         mode='min',
+                                                         save_best_only=True)
+
+        # early stopping if not changing for 50 epochs
+        early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                      patience=early_stop_patience)
+
+        # reduce learning rate
+        reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss',
+                                                         factor=0.1,
+                                                         patience=reduce_lr_patience,
+                                                         verbose=1,
+                                                         min_lr=0.00001)
+
+        profiler = tf.keras.callbacks.TensorBoard(log_dir='logs',
+                                                  histogram_freq=1,
+                                                  profile_batch='15,25')
+        return cp_callback, early_stop, reduce_lr, profiler
+
+    def train(self, epochs=300, early_stop_patience=100, reduce_lr_patience=50):
+        xs = self.train_ds[0].astype('float32')
+        ys = self.train_ds[1].astype('float32')
+        val_xs = self.val_ds[0].astype('float32')
+        val_ys = self.val_ds[1].astype('float32')
+
+        start = time.time()
+        callbacks = self.prepare_callbacks(early_stop_patience, reduce_lr_patience)
+
+        source_dataset = tf.data.Dataset.from_tensor_slices((xs, ys)).shuffle(len(xs)).batch(self.batch_size)
+        # target_dataset = tf.data.Dataset.from_tensor_slices(xs_real).shuffle(len(x_real)).repeat().batch(self.batch_size)
+        # train_dataset = tf.data.Dataset.zip((source_dataset, target_dataset))
+
+        val_source_dataset = tf.data.Dataset.from_tensor_slices((val_xs, val_ys)).batch(self.batch_size)
+        # # val_target_dataset = tf.data.Dataset.from_tensor_slices(val_xs).shuffle(BUFFER_SIZE).repeat().batch(self.batch_size)
+        # val_dataset = tf.data.Dataset.zip((val_source_dataset, val_target_dataset))
+
+        history = self.model.fit(source_dataset,
+                                 batch_size=self.batch_size,
+                                 epochs=epochs,
+                                 callbacks=callbacks,
+                                 validation_data=val_source_dataset,
+                                 shuffle=True)
+
+        end = time.time()
+        print('\ntotal time spent for training: {}[min]'.format((end-start)/60))
+
+
+def visualize_result(f_prediction, f_label, rgb, filename=None):
+    plt.savefig('prediction.png')
+    p = plt.imread('prediction.png')[:, :, :3]
+    if f_label is None:
+        p = cv2.resize(p, (1440, 360))
+        res = np.concatenate([rgb, p], axis=1)
+    if f_label is not None:
+        forcemap.plot_force_map(f_label, 'ground truth')
+        plt.savefig('ground_truth.png')
+        g = plt.imread('ground_truth.png')[:, :, :3]
+        pg = np.concatenate([p, g], axis=0)
+        rgb2 = np.ones((800, 512, 3))
+        rgb2[220:580] = rgb
+        res = np.concatenate([rgb2, pg], axis=1)
+    fig = plt.figure(figsize=(10, 4))
+    ax = fig.add_subplot(111)
+    ax.axis('off')
+    plt.imshow(res)
+    if filename is not None:
+        plt.savefig(filename)
+    plt.show()
+
+
+class Tester:
+    def __init__(self, model, test_data, checkpoint_file, runs_directory=None):
+        self.model = model
+        d = runs_directory if runs_directory else os.path.join(os.path.dirname(os.getcwd()), 'runs')
+        self.checkpoint_path = os.path.join(d, checkpoint_file, 'cp.ckpt')
+        print('load weights from ', self.checkpoint_path)
+        self.model.load_weights(self.checkpoint_path)
+        self.test_data = test_data
+
+    def predict(self, n):
+        xs = self.test_data[0][n:n+1]
+        # ys = self.test_data[1][n:n+1]
+        y_pred = self.model.predict(xs)
+        plt.imshow(xs[0])
+        forcemap.plot_force_map(y_pred[0], 'prediction')
+        plt.show()
+
+    def predict_segmentation_mask(self, n):
+        xs = self.test_data[0][n:n+1]
+        ys = self.test_data[1][n:n+1]
+        y_pred_logits = self.model.predict(xs)
+        plt.figure()
+        plt.imshow(xs[0])
+        y_pred = tf.cast(tf.argmax(y_pred_logits, axis=-1), dtype=tf.uint8)
+        plt.figure()
+        plt.imshow(y_pred[0])
+        plt.figure()
+        plt.imshow(ys[0])
+        plt.show()
+        return xs[0], ys[0], y_pred[0]
+
+    def predict_force_from_rgb(self, n, visualize=True):
+        if type(self.test_data) is tuple:
+            xs = self.test_data[0][n:n+1]
+            force_label = self.test_data[1][n]
+        else:
+            xs = self.test_data[n:n+1]
+            force_label = None
+        y_preds = self.model.predict(xs)
+        y_pred_forces = y_preds
+        visualize_result(y_pred_forces[0], force_label, xs[0], 'result{:05d}.png'.format(n))
+        return y_pred_forces[0], force_label, xs[0]
+
+    def predict_force_from_rgb_with_img(self, rgb):
+        xs = np.expand_dims(rgb, axis=0)
+        y_preds = self.model.predict(xs)
+        y_pred_forces = y_preds
+        forcemap.plot_force_map(y_pred_forces[0], 'prediction')
+        plt.show()
+        return y_pred_forces
+
+    def predict_force_from_depth_seg(self, n, rgb):
+        xs_depth = self.test_data[0][0][n:n+1]
+        xs_seg = self.test_data[0][1][n:n+1]
+        xs = xs_depth, xs_seg
+        y_preds = self.model.predict(xs)
+        y_pred_forces = y_preds
+        force_label = self.test_data[1][n]
+
+        visualize_result(y_pred_forces[0], force_label, rgb[n], 'result{:05d}.png'.format(n))
+        return y_pred_forces[0], force_label, rgb[n]
+
+
+def show_bin_state(fcam, bin_state, fmap, draw_fmap=True, draw_force_gradient=False):
+    fv = np.zeros((40, 40, 40))
+    fv[:, :, :20] = fmap
+    fmap.set_values(fv)
+    viewer.publish_bin_state(bin_state, fmap, draw_fmap=draw_fmap, draw_force_gradient=draw_force_gradient)
+
+
+task = 'test-real'
+model_file = 'ae_cp.basket-filling2.model_resnet.20221202165608'
+
+if task == 'train':
+    model = fe.model_rgb_to_fmap_res50()
+    train_data, valid_data = dl.load_data_for_rgb2fmap(train_mode=True)
+    trainer = Trainer(model, train_data, valid_data)
+elif task == 'adaptation':
+    model = fe.model_rgb_to_fmap_res50()
+    train_data, valid_data = dl.load_data_for_rgb2fmap_with_real(train_mode=True)
+    trainer = Trainer(model, train_data, valid_data)
+elif task == 'test':
+    model = fe.model_rgb_to_fmap_res50()
+    test_data = dl.load_data_for_rgb2fmap(test_mode=True)
+    tester = Tester(model, test_data, model_file)
+elif task == 'test-real':
+    model = fe.model_rgb_to_fmap_res50()
+    test_data = dl.load_real_data_for_rgb2fmap(test_mode=True)
+    tester = Tester(model, test_data, model_file)
