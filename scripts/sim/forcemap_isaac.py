14db414073d87d5e1b8fac1be669e0ba57d6deb7
Ryo Hanai <ryhanai@gmal.com>
2023-04-19 16:38:20 +0900

    code to get contact information in Isaac

---
+++ scripts/sim/forcemap_isaac.py
@@ -0,0 +1,217 @@
+
+from omni.isaac.kit import SimulationApp
+
+simulation_app = SimulationApp({"headless": False})
+
+from omni.isaac.core import World
+from omni.isaac.sensor import ContactSensor
+from omni.isaac.core import SimulationContext
+from omni.isaac.core.utils.nucleus import get_assets_root_path
+from omni.isaac.core.utils.stage import add_reference_to_stage
+import numpy as np
+import os
+import omni
+
+from pxr import Gf, UsdGeom
+from omni.isaac.core.objects import VisualSphere
+import carb
+import omni.usd
+import gc
+
+
+# assets_root_path = get_assets_root_path()
+# asset_path = assets_root_path + "/Isaac/Robots/Franka/franka_alt_fingers.usd"
+
+my_world = World(stage_units_in_meters=1.0)
+my_world.scene.add_default_ground_plane()
+
+asset_path = os.environ["HOME"] + "/Downloads/Collected_ycb_piled_scene/ycb_piled_scene.usd"
+# asset_path = os.environ["HOME"] + "/Program/moonshot/ae_lstm/specification/meshes/objects/seria_basket/seria_basket.usd"
+
+simulation_context = SimulationContext()
+# add_reference_to_stage(usd_path=asset_path, prim_path="/World")
+
+
+# follow_sphere = my_world.scene.add(
+#     VisualSphere(
+#         name="follow_sphere", prim_path="/World/FollowSphere", radius=0.02, color=np.array([0.7, 0.0, 0.7])
+#     )
+# )
+
+objects = ['_09_gelatin_box', '_61_foam_brick']
+
+
+def set_translate(prim, new_loc):
+    properties = prim.GetPropertyNames()
+    if "xformOp:translate" in properties:
+        translate_attr = prim.GetAttribute("xformOp:translate")
+
+        translate_attr.Set(new_loc)
+    elif "xformOp:translation" in properties:
+        translation_attr = prim.GetAttribute("xformOp:translate")
+        translation_attr.Set(new_loc)
+    elif "xformOp:transform" in properties:
+        transform_attr = prim.GetAttribute("xformOp:transform")
+        matrix = prim.GetAttribute("xformOp:transform").Get()
+        matrix.SetTranslateOnly(new_loc)
+        transform_attr.Set(matrix)
+    else:
+        xform = UsdGeom.Xformable(prim)
+        xform_op = xform.AddXformOp(UsdGeom.XformOp.TypeTransform, UsdGeom.XformOp.PrecisionDouble, "")
+        xform_op.Set(Gf.Matrix4d().SetTranslate(new_loc))
+
+
+prim = my_world.stage.GetPrimAtPath('/World')
+# prim = my_world.stage.DefinePrim('/World/ycb', "Xform")
+prim.GetReferences().AddReference(asset_path)
+ch_prim = my_world.stage.GetPrimAtPath('/World/seria_basket')
+set_translate(ch_prim, Gf.Vec3d([0,0,1.]))
+
+
+contact_sensors = []
+for o in objects:
+    contact_sensors.append(
+        my_world.scene.add(
+            ContactSensor(
+                prim_path="/World/{}/contact_sensor".format(o),
+                name="{}_cs".format(o),
+                min_threshold=0,
+                max_threshold=10000000,
+                radius=0.5,
+                translation=np.array([0, 0, 0])
+            )
+        )
+    )
+
+
+# need to initialize physics getting any articulation..etc
+simulation_context.initialize_physics()
+# simulation_context.play()
+
+# for i in range(1000):
+#     simulation_context.step(render=True)
+
+# contact_sensors[0].add_raw_contact_data_to_frame()
+
+
+def read_contact_sensor(contact_sensor):
+    csif = contact_sensor._contact_sensor_interface
+    cs_raw_data = csif.get_contact_sensor_raw_data(contact_sensor.prim_path)
+    backend_utils = simulation_context.backend_utils
+    device = simulation_context.device
+
+    current_frame = []
+    for i in range(len(cs_raw_data)):
+        contact_point = dict()
+        contact_point["body0"] = csif.decode_body_name(int(cs_raw_data["body0"][i]))
+        contact_point["body1"] = csif.decode_body_name(int(cs_raw_data["body1"][i]))
+
+        contact_point["position"] = backend_utils.create_tensor_from_list(
+                                    [
+                                        cs_raw_data["position"][i][0],
+                                        cs_raw_data["position"][i][1],
+                                        cs_raw_data["position"][i][2],
+                                    ],
+                                    dtype="float32",
+                                    device=device,
+                                    )
+        contact_point["normal"] = backend_utils.create_tensor_from_list(
+                                    [
+                                        cs_raw_data["normal"][i][0],
+                                        cs_raw_data["normal"][i][1],
+                                        cs_raw_data["normal"][i][2]
+                                    ],
+                                    dtype="float32",
+                                    device=device,
+                                    )
+        contact_point["impulse"] = backend_utils.create_tensor_from_list(
+                                    [
+                                        cs_raw_data["impulse"][i][0],
+                                        cs_raw_data["impulse"][i][1],
+                                        cs_raw_data["impulse"][i][2],
+                                    ],
+                                    dtype="float32",
+                                    device=device,
+                                    )
+        current_frame.append(contact_point)
+    return current_frame
+
+
+def get_bin_state():
+    prim_path = "/World/_09_gelatin_box/contact_sensor"
+    obj = my_world.scene.get_object(prim_path)
+    return obj.get_world_pose()
+
+
+def drop_an_object(name='_09_gelatin_box'):
+    prim = object_prims[name]
+    set_world_pose(prim)
+
+
+stage = omni.usd.get_context().get_stage()
+object_prims = {}
+for o in objects:
+    object_prims[o] = stage.GetPrimAtPath('/World/{}'.format(o))
+
+
+def set_world_pose(prim, position=Gf.Vec3d(0, 0, 1.5)):
+    xform = UsdGeom.Xformable(prim)
+    # xform.ClearXformOpOrder()
+    transform = xform.AddTransformOp()
+    mat = Gf.Matrix4d()
+    mat.SetTranslateOnly(position)
+    mat.SetRotateOnly(Gf.Rotation(Gf.Vec3d(0, 0, 1), 0))
+    transform.Set(mat)
+
+
+# prim = object_prims[objects[0]]
+# set_world_pose(prim)
+
+
+stable = False
+cnt = 0
+def is_scene_stable():
+    global cnt
+    if cnt > 500:
+        cnt = 0
+        return True
+    else:
+        cnt += 1
+        return False
+
+
+sr = my_world.scene._scene_registry
+print(sr._rigid_objects, sr._geometry_objects)
+
+
+while simulation_app.is_running():
+    my_world.step(render=True)
+    
+    if my_world.is_playing():
+
+        # while o in objects:
+        #     if stable:
+        #         stable = False
+    
+        #     else:
+        #         if is_scene_stable():
+        #             stable = True
+
+            # print(contact_sensors[0].get_current_frame())
+            # print(contact_sensors[0]._contact_sensor_interface.get_contact_sensor_raw_data(contact_sensors[0].prim_path))
+
+        print(read_contact_sensor(contact_sensors[0]))
+        
+        # prim = object_prims[objects[0]]
+        # pose = omni.usd.utils.get_world_transform_matrix(prim)
+        # trans = pose.ExtractTranslation()
+        # quat = pose.ExtractRotation().GetQuaternion()
+        # print(trans, quat)
+
+        if my_world.current_time_step_index == 0:
+            my_world.reset()
+
+#     simulation_context.step(render=True)
+
+# simulation_context.stop()
+simulation_app.close()
