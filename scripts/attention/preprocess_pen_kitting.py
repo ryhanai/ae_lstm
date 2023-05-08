4da2fd0138149c4e7b28d93d75fd6725d4482b6d
Ryo Hanai <ryo.hanai@aist.go.jp>
2023-05-08 23:31:02 +0900

    update preprocessing

--- scripts/attention/preprocess_pen_kitting.py
+++ scripts/attention/preprocess_pen_kitting.py
@@ -6,39 +6,76 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-episode_number = 1
-episode = pd.read_pickle(
-    os.path.join(os.environ['HOME'],
-                 'Dataset/dataset2/pen-kitting-real23-5-2',
-                 str(episode_number),
-                 'states.pkl')
-)
+def read_episode(episode_number):
+    return pd.read_pickle(os.path.join(os.environ['HOME'],
+                                       'Dataset/dataset2/pen-kitting-real230508',
+                                       str(episode_number),
+                                       'states.pkl'))
 
 
-def parse(episode):
-    frame = episode[0]
-    img, joint_command, arm_joint_positions, gripper_joint_positions = frame
+episodes = [read_episode(n) for n in range(48)]
+episode = episodes[2]
 
 
-def get_time_labels(episode):
+arm_joint_order = [
+    'shoulder_pan_joint',
+    'shoulder_lift_joint',
+    'elbow_joint',
+    'wrist_1_joint',
+    'wrist_2_joint',
+    'wrist_3_joint',
+]
 
 
-def reorder_joint_values(joint_positions):
+# frame[0] = image
+# frame[1] = joint action
+# frame[2] = joint observation
+# frame[3] = gripper joint observation
+
+
+def get_time_labels(episode, start_from_zero=True):
+    tms = np.array([f[1].header.stamp.to_sec() for f in episode])
+    if start_from_zero:
+        return tms - tms[0]
+    else:
+        return tms
+
+
+def reorder_joint_values(joint_names, joint_positions):
+    d = dict(zip(joint_names, joint_positions))
+    return [d[jo] for jo in arm_joint_order]         
+
+
+def get_arm_joint_observation(episode):
+    return [reorder_joint_values(f[2].name, f[2].position) for f in episode]
+
+
+def get_arm_joint_action(episode):
+    return [reorder_joint_values(f[1].joint_names, f[1].points[0].positions) for f in episode]
+
+
+def get_gripper_joint_action(episode):  # observation = action in gripper
+    return [f[3].position for f in episode]
+
+
+def get_action(episode):
+    ao = get_arm_joint_action(episode)
+    go = get_gripper_joint_action(episode)
+    return np.concatenate([ao, go], axis=1)
+
+
+def plot_action(episode):
+    tms = get_time_labels(episode)
+    a = get_action(episode)
+    fig, ax = plt.subplots(facecolor="w")
+    for i in range(7):
+        ax.plot(tms, a[:, i], label=f'joint{i}')
+    ax.legend()
+    plt.show()
+
+
+def compute_joint_range():
     pass
-
-
-def get_joint_observation(episode):
-    obs = [f[2].position[2] for f in episode]
-
-
-def get_joint_action(episode):
-    tms = [f[1].header.stamp.to_sec() for f in episode]
-    act = [reorder_joint_values(f[1].points[0].positions[0]) for f in episode]
-    # plt.plot(tms, act[:,0])
-    # plt.plot(tms, act[:,1])
-    # plt.show()
-
-
 
 # check synchronization accuracy
 
@@ -47,4 +84,3 @@ def get_joint_action(episode):
 # check arm joint delay (compare action/observation for single arm joint)
 
 # gen input data for training a model
-
