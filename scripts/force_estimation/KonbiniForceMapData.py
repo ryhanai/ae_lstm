aa82a94ed088787e2a522811bc1c18a5ade36da0
vtacuser <ryo.hanai@aist.go.jp>
2023-06-01 18:20:17 +0900

    implementing forcemap dataset for PyTorch

--- scripts/force_estimation/KonbiniForceMapData.py
+++ scripts/force_estimation/KonbiniForceMapData.py
@@ -6,7 +6,32 @@ from urllib.error import URLError
 import torch
 from torchvision import transforms
 from torch.utils.data import Dataset
-# from eipl.utils import normalization
+from eipl.utils import normalization
+
+import pandas as pd
+import cv2
+
+def curate_dataset(num_samples=100):
+    input_dir = os.path.join(os.path.expanduser('~'), 'Dataset/dataset2/konbini-stacked/')
+    output_dir = os.path.join(os.path.expanduser('~'), 'Dataset/dataset2/konbini-stacked-c/')
+    all_ids = range(num_samples)
+    train_ids, validation_ids, test_ids = np.split(all_ids, [int(len(all_ids)*0.75), int(len(all_ids)*0.875)])
+
+    def f(ids, output_dir):
+        fmaps = []
+        rgbs = []
+        for i in ids:
+            print(i)
+            fmap = pd.read_pickle(os.path.join(input_dir, f'force_zip{i:05}.pkl'), compression='zip')
+            fmaps.append(fmap.astype('float32'))
+            rgb = cv2.cvtColor(cv2.imread(os.path.join(input_dir, f'rgb{i:05}.jpg')), cv2.COLOR_BGR2RGB)
+            rgbs.append(cv2.resize(rgb, (640, 320)))
+        pd.to_pickle(np.array(fmaps), os.path.join(output_dir, f'force_bz2.pkl'), compression='bz2')
+        pd.to_pickle(np.array(rgbs), os.path.join(output_dir, f'rgb_bz2.pkl'), compression='bz2')
+
+    f(train_ids, os.path.join(output_dir, 'train'))
+    f(validation_ids, os.path.join(output_dir, 'validation'))
+    f(test_ids, os.path.join(output_dir, 'test'))
 
 
 class KonbiniRandomScene:
@@ -25,76 +50,48 @@ class KonbiniRandomScene:
                   minmax=[0.1, 0.9],
                   stdev=0.02,
                   img_format='CWH',
-                  root_dir=os.path.join( os.path.expanduser('~'), 'Dataset/dataset2/konbini-stacked/'),        
-                  download=False):
+                  root_dir=os.path.join( os.path.expanduser('~'), 'Dataset/dataset2/'),
+                  ):
         
         self.data_type  = data_type
         self.minmax     = minmax
         self.stdev      = stdev
         self.img_format = img_format
-        self.task_name  = 'force_prediction'
+        self.task_name  = 'konbini-stacked-c'
         self.root_dir   = root_dir
-        self.tar_data_path = os.path.join(self.root_dir, self.task_name+'.tar' )
         self.mirror_url = ''
 
-        if download:
-            self._download()
-
-        # load npy data
-        self.joint_bounds = self._load_bounds()
-        self.images_raw, self.joints_raw = self._load_data()
+        self.images_raw, self.forces_raw = self._load_data()
 
         # normalization
-        # images: (data_num, seq_len, c, h, w)
-        # joints: (data_num, seq_len, 8)
+        # images: (data_num, c, h, w)
+        # forces: (data_num, c, h, w)
         _images = self._normalization(self.images_raw.astype(np.float32), (0.0, 255.0) )
-        _joints = self._normalization(self.joints_raw.astype(np.float32), self.joint_bounds )
         self.images = torch.from_numpy(_images).float()
-        self.joints = torch.from_numpy(_joints).float()
-
-    def _check_exists(self):
-        return os.path.isfile( self.tar_data_path )
-
-    def _download(self):
-        """Download the data if it doesn't exist already."""
-        os.makedirs(self.root_dir, exist_ok=True)
-
-        # download files
-        try:
-            if not self._check_exists():
-                print(f"Downloading {self.mirror_url}")
-                urllib.request.urlretrieve(self.mirror_url, self.tar_data_path)
-
-            with tarfile.open(self.tar_data_path, 'r:tar') as tar:
-                tar.extractall(path=self.root_dir)
-
-        except URLError as error:
-            raise RuntimeError(f"Error downloading")
-
-    def _load_bounds(self):
-        joint_bounds = np.load( os.path.join(self.root_dir, self.task_name, 'joint_bounds.npy') )
-        return joint_bounds
+        # forcemap is saved in log-scale in the original dataset, so no need to normalize
+        self.forces = self.forces_raw
 
     def _load_data(self):
-        joints = np.load( os.path.join(self.root_dir, self.task_name, self.data_type, 'joints.npy') )
-        images = np.load( os.path.join(self.root_dir, self.task_name, self.data_type, 'images.npy') )
+        images = pd.read_pickle( os.path.join(self.root_dir, self.task_name, self.data_type, 'rgb_bz2.pkl'), compression='bz2' )
+        forces = pd.read_pickle( os.path.join(self.root_dir, self.task_name, self.data_type, 'force_bz2.pkl'), compression='bz2' )
         
         if self.img_format == 'CWH':
-            images = images.transpose(0,1,4,2,3)
+            images = images.transpose(0,3,1,2)
+            forces = forces.transpose(0,3,1,2)
 
-        return images, joints
+        return images, forces
 
     def _normalization(self, data, bounds):
         return normalization(data, bounds, self.minmax)
 
     def get_data(self, device=None):
-        return self.images.to(device), self.joints.to(device)
+        return self.images.to(device), self.forces.to(device)
 
     def get_raw_data(self):
-        return self.images_raw, self.joints_raw
+        return self.images_raw, self.forces_raw
 
 
-class GraspBottleDataset(Dataset, GraspBottle):
+class KonbiniRandomImageDataset(Dataset, KonbiniRandomScene):
     """AIREC_sample dataset.
 
     Args:
@@ -112,54 +109,7 @@ class GraspBottleDataset(Dataset, GraspBottle):
                   img_format='CWH',
                   root_dir=os.path.join( os.path.expanduser('~'), '.eipl/'),
                   download=True):
-        GraspBottle.__init__(self,
-                  data_type=data_type,
-                  minmax=minmax,
-                  stdev=stdev,
-                  img_format=img_format,
-                  root_dir=root_dir,
-                  download=download)
-        
-        self.transform = transforms.ColorJitter(contrast=0.5, brightness=0.5, saturation=0.1)
-    
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        
-        # normalization and convert numpy array to torch tensor
-        y_img   = self.images[idx]
-        y_joint = self.joints[idx]
-
-        # apply base image transformations to image
-        x_img = self.transform( self.images[idx] )
-        x_img = x_img + torch.normal(mean=0, std=self.stdev, size=x_img.shape)
-
-        # apply gaussian noise to joint angles
-        x_joint = self.joints[idx] + torch.normal(mean=0, std=self.stdev, size=y_joint.shape)
-
-        return [ [x_img, x_joint], [y_img, y_joint] ]
-
-
-class GraspBottleImageDataset(GraspBottleDataset):
-    """AIREC_sample dataset.
-
-    Args:
-        data_type (string):        Set the data type (train/test) .
-        minmax (float, optional):  Set normalization range, default is [0.1,0.9].
-        root (string, optional):   Root directory of the data set, default is saved in the '~/epil/'.
-        download (bool, optional): If True, downloads the dataset from the internet and
-                                   puts it in root directory. If dataset is already downloaded, it is not
-                                   downloaded again.
-    """
-    def __init__( self,
-                  data_type,
-                  minmax=[0.1, 0.9],
-                  stdev=0.02,
-                  img_format='CWH',
-                  root_dir=os.path.join( os.path.expanduser('~'), '.eipl/'),
-                  download=True):
-        GraspBottleDataset.__init__(self,
+        KonbiniRandomScene.__init__(self,
                   data_type=data_type,
                   minmax=minmax,
                   stdev=stdev,
@@ -193,7 +143,6 @@ class GraspBottleImageDataset(GraspBottleDataset):
         return [ x_img, y_img ]
 
 
-
 if __name__ == '__main__':
     import cv2
     import time
@@ -201,25 +150,25 @@ if __name__ == '__main__':
     from eipl.utils import deprocess_img, tensor2numpy
     
     # load data
-    data_loader = GraspBottleDataset('train', minmax=[0.1, 0.9], img_format='CWH')
-    x_data, y_data = data_loader[1]
-    images = np.concatenate( (x_data[0], y_data[0]), axis=3 )
-    images = images.transpose(0,2,3,1)
+    data_loader = KonbiniRandomScene('train', minmax=[0.1, 0.9], img_format='CWH')
+    # x_data, y_data = data_loader[1]
+    # images = np.concatenate( (x_data[0], y_data[0]), axis=3 )
+    # images = images.transpose(0,2,3,1)
 
-    # tensor to numpy
-    images = deprocess_img( images, 0.1, 0.9 )
+    # # tensor to numpy
+    # images = deprocess_img( images, 0.1, 0.9 )
 
-    # plot joint angles
-    plt.plot(x_data[1], linestyle='dashed', c='k')
-    plt.plot(y_data[1])
-    plt.show()
+    # # plot joint angles
+    # plt.plot(x_data[1], linestyle='dashed', c='k')
+    # plt.plot(y_data[1])
+    # plt.show()
     
-    # plt images
-    for i in range(images.shape[0]):
-        cv2.imshow('Video', images[i,:,:,::-1])
+    # # plt images
+    # for i in range(images.shape[0]):
+    #     cv2.imshow('Video', images[i,:,:,::-1])
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        time.sleep(0.05)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    #     time.sleep(0.05)
     
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
