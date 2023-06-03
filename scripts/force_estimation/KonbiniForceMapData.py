bug fix, normalization of force

scripts/force_estimation/KonbiniForceMapData.py
@@ -34,21 +34,26 @@ def curate_dataset(num_samples=100):
 
     height = 14 * 24
     width = height * 2
-    def f(ids, output_dir):
+    def f(ids, output_dir, data_type):
         fmaps = []
         rgbs = []
         for i in ids:
             print(i)
             fmap = pd.read_pickle(os.path.join(input_dir, f'force_zip{i:05}.pkl'), compression='zip')
+            fmap = fmap[:,:,:30]
             fmaps.append(fmap.astype('float32'))
             rgb = cv2.cvtColor(cv2.imread(os.path.join(input_dir, f'rgb{i:05}.jpg')), cv2.COLOR_BGR2RGB)
             rgbs.append(cv2.resize(rgb, (width, height)))
-        pd.to_pickle(np.array(fmaps), os.path.join(output_dir, f'force_bz2.pkl'), compression='bz2')
-        pd.to_pickle(np.array(rgbs), os.path.join(output_dir, f'rgb_bz2.pkl'), compression='bz2')
+        fmaps = np.array(fmaps)
+        rgbs = np.array(rgbs)
+        pd.to_pickle(fmaps, os.path.join(output_dir, data_type, f'force_bz2.pkl'), compression='bz2')
+        pd.to_pickle(rgbs, os.path.join(output_dir, data_type, f'rgb_bz2.pkl'), compression='bz2')
+        if data_type == 'train':
+            np.save(os.path.join(output_dir, f'force_bounds.npy'), np.array([np.min(fmaps), np.max(fmaps)]))
 
-    f(train_ids, os.path.join(output_dir, 'train'))
-    f(validation_ids, os.path.join(output_dir, 'validation'))
-    f(test_ids, os.path.join(output_dir, 'test'))
+    f(train_ids, output_dir, 'train')
+    f(validation_ids, output_dir, 'validation')
+    f(test_ids, output_dir, 'test')
 
 
 class KonbiniRandomScene:
@@ -78,25 +83,27 @@ class KonbiniRandomScene:
         self.root_dir   = root_dir
         self.mirror_url = ''
 
-        self.images_raw, self.forces_raw = self._load_data()
+        self.images_raw, self.forces_raw, self.force_bounds = self._load_data()
 
         # normalization
         # images: (data_num, c, h, w)
         # forces: (data_num, c, h, w)
-        _images = self._normalization(self.images_raw.astype(np.float32), (0.0, 255.0) )
+        _images = self._normalization(self.images_raw.astype(np.float32), (0.0, 255.0))
         self.images = torch.from_numpy(_images).float()
-        # forcemap is saved in log-scale in the original dataset, so no need to normalize
-        self.forces = self.forces_raw
+        # forcemap is saved in log-scale in the original dataset
+        _forces = self._normalization(self.forces_raw, self.force_bounds)
+        self.forces = torch.from_numpy(_forces).float()
 
     def _load_data(self):
         images = pd.read_pickle( os.path.join(self.root_dir, self.task_name, self.data_type, 'rgb_bz2.pkl'), compression='bz2' )
         forces = pd.read_pickle( os.path.join(self.root_dir, self.task_name, self.data_type, 'force_bz2.pkl'), compression='bz2' )
+        force_bounds = np.load( os.path.join(self.root_dir, self.task_name, 'force_bounds.npy') )
         
         if self.img_format == 'CWH':
             images = images.transpose(0,3,1,2)
             forces = forces.transpose(0,3,1,2)
 
-        return images, forces
+        return images, forces, force_bounds
 
     def _normalization(self, data, bounds):
         return normalization(data, bounds, self.minmax)
@@ -135,26 +142,27 @@ class KonbiniRandomSceneDataset(Dataset, KonbiniRandomScene):
 
         # self.images_flatten = self.images.reshape( ((-1,) + self.images.shape[2:]) )
 
-        self.transform_affine = transforms.Compose([
-            transforms.RandomAffine(degrees=(-3, 3), translate=(0.03, 0.03)),
-            transforms.ColorJitter(hue=0.1, saturation=0.1),
-            transforms.RandomAutocontrast(),
-            # transforms.RandomHorizontalFlip(),
-            # transforms.RandomVerticalFlip()
-            ])
-        self.transform_noise  = transforms.Compose([
-            transforms.ColorJitter(contrast=0.1, brightness=0.1),
-            ])
+        # self.transform_affine = transforms.Compose([
+        #     transforms.RandomAffine(degrees=(-3, 3), translate=(0.03, 0.03)),
+        #     transforms.ColorJitter(hue=0.1, saturation=0.1),
+        #     transforms.RandomAutocontrast(),
+        #     # transforms.RandomHorizontalFlip(),
+        #     # transforms.RandomVerticalFlip()
+        #     ])
+        # self.transform_noise  = transforms.Compose([
+        #     transforms.ColorJitter(contrast=0.1, brightness=0.1),
+        #     ])
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
         # normalization and convert numpy array to torch tensor
-        y_img = self.transform_affine(self.images[idx])
+        # y_img = self.transform_affine(self.images[idx])
         # apply base image transformations to image                                                                                     
-        x_img = self.transform_noise(y_img) + torch.normal(mean=0, std=self.stdev, size=y_img.shape)
+        # x_img = self.transform_noise(y_img) + torch.normal(mean=0, std=self.stdev, size=y_img.shape)
 
+        x_img = self.images[idx]
         y_force = self.forces[idx]
         return x_img, y_force
 

scripts/force_estimation/force_estimation_v4.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from torchinfo import summary
 import cv2
+import torchvision.transforms as T
+
 
 # dinov2_vits14 = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
 # img = cv2.imread('/home/ryo/Dataset/dataset2/pen-kitting-real/1/image_frame00000.jpg')
@@ -15,11 +17,25 @@ class ForceEstimationDINOv2(nn.Module):
         input: (3, 336, 672): width and height must be a multple of 14
         output: (40, 120, 160)
     """
-    def __init__(self):
+    def __init__(self, device=0, fine_tune_encoder=True):
         super().__init__()
+
+        self.stdev = 0.02
+        self.device = device
+
+        self.augmenter = T.Compose([
+            # T.ToTensor(),
+            T.RandomAffine(degrees=(-3, 3), translate=(0.03, 0.03)),
+            T.ColorJitter(hue=0.1, saturation=0.1),
+            T.RandomAutocontrast(),
+            T.ColorJitter(contrast=0.1, brightness=0.1),
+        ])
 
         # image -> torch.Size([1, 384])
         self.encoder = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
+        if not fine_tune_encoder:
+            for p in self.encoder.parameters():
+                p.require_drad = False
 
         # self.decoder = nn.Sequential(
         #     nn.Linear(384, 50), nn.BatchNorm1d(50), nn.ReLU(True),
@@ -38,8 +54,17 @@ class ForceEstimationDINOv2(nn.Module):
         )
 
     def forward(self, x):
+        x = self.augmenter(x) + torch.normal(mean=0, std=self.stdev, size=x.shape).to(self.device)
         return self.decoder(self.encoder(x))
 
 
+class ForceEstimationResNet(nn.Module):
+    def __init__(self, fine_tune_encoder=True):
+        super().__init__()
+
+    def forward(self, x):
+        return self.decoder(self.encoder(self.augmenter(x)))
+
+# print(summary(self, input_size=(32, 3, 224, 224)))
 # model = ForceEstimationDINOv2()
-# print(summary(model, input_size=(32, 3, 224, 224)))
+