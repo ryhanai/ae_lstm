0b85e5cef367524c58d041c566e1519e0f80749a
Ryo Hanai <ryo.hanai@aist.go.jp>
2023-07-01 18:35:52 +0900

    moved hard-coded parameters into parameter files

--- scripts/force_estimation/test_torch.py
+++ scripts/force_estimation/test_torch.py
@@ -1,20 +1,19 @@
 import os
 import time
-import torch
-# import torch.nn as nn
 import argparse
 import matplotlib.pylab as plt
-# import matplotlib.animation as anim
 import numpy as np
 import pandas as pd
+import json
 
+import torch
 from torchinfo import summary
 from eipl_utils import tensor2numpy
 from eipl_print_func import print_info
-from eipl_arg_utils import restore_args
+
 from KonbiniForceMapData import *
 from SeriaBasketForceMapData import *
-from force_estimation_v4 import ForceEstimationResNetSeriaBasket
+from force_estimation_v4 import *
 
 import forcemap
 from force_estimation import force_distribution_viewer
@@ -24,10 +23,10 @@ import torch._dynamo
 torch._dynamo.config.verbose = True
 torch._dynamo.config.suppress_errors = True
 
+
 # argument parser
 parser = argparse.ArgumentParser()
-parser.add_argument('--filename', type=str, default=None)
-# parser.add_argument('--idx',      type=str, default='0' )
+parser.add_argument('--filename', type=str, default='log/20230627_1730_52/CAE.pth')
 parser.add_argument('--dataset_path', type=str, default='./basket-filling3-c-1k')
 args = parser.parse_args()
 
@@ -35,47 +34,37 @@ args = parser.parse_args()
 # if args.filename is None and not args.pretrained:
 #     assert False, 'Please set filename or pretrained'
 
-print_info("load pretrained weight")
-# args.filename = os.path.join('log', '20230606_1017_32', 'CAE.pth' ) # 1k
-# args.filename = os.path.join('log', '20230606_1854_41', 'CAE.pth' ) # 4k resenc + resdec, compiled
-# args.filename = os.path.join('log', '20230612_1057_39', 'CAE.pth' ) # 1k resenc + resdec
-# args.filename = os.path.join('log', '20230612_1914_05', 'CAE.pth' ) # 1k resenc + resdec, dino fine-tune
-# args.filename = os.path.join('log', '20230612_1601_40', 'CAE.pth' ) # 1k resenc + resdec, no fine-tune
-args.filename = os.path.join('log', '20230627_1730_52', 'CAE.pth' ) # basket-filling
+# restore model parameters
+with open(os.path.join(os.path.split(args.filename)[0], 'args.json'), 'r') as f:
+    model_params = json.load(f)
+with open(os.path.join(args.dataset_path, 'params.json'), 'r') as f:
+    dataset_params = json.load(f)
 
-# restore parameters
-dir_name = os.path.split(args.filename)[0]
-params = restore_args( os.path.join(dir_name, 'args.json') )
-# idx    = int(args.idx)
-
+print_info(f'loading pretrained weight [{args.filename}]')
 ckpt = torch.load(args.filename)
 # ckpt = torch.load(args.filename, map_location=torch.device('cpu'))
 
+data_loader = dataset_params['data loader']
+print_info(f'loading test data [{data_loader}]')
+test_data = globals()[data_loader]('test')
 
-dataset_class = pd.read_pickle(os.path.join(args.dataset_path, 'params.pkl'))['dataset class']
-print_info('loading test data ...')
-test_data = globals()[dataset_class]('validation')
-print_info('build model and load weights ...')
-model_class = 'ForceEstimationResNetSeriaBasket'  # this name is given by 'args.json'
-model = globals()[model_class](device='cpu')  # for fast inference, use GPU
+model_class = model_params['model']
+print_info(f'build model and load weights [{model_class}]')
+model = globals()[model_class]()
 model.load_state_dict(ckpt['model_state_dict'])
 
 
 class Tester:
-    def __init__(self, test_data, model, params):
+    def __init__(self, test_data, model, dataset_params):
         self._device = 'cuda'
         self.test_data = test_data
         self._model = model
         self._model.to(self._device)
         self._model.eval()
         print(summary(self._model, input_size=(16, 3, 336, 672)))
-        # load weight and compile
         # self._model = torch.compile(self._model)
-
-        forcemap_scene = 'seria_basket'  # this is identified by dataset
+        forcemap_scene = dataset_params['forcemap']
         self._fmap = forcemap.GridForceMap(forcemap_scene)
-
-        # minmax = [params['vmin'], params['vmax']]
 
     def predict_by_index(self, idx, show_result=True, log_scale=True, show_bin_state=True):
         batch = torch.unsqueeze(self.test_data[idx][0], 0).to(self._device)
@@ -112,7 +101,7 @@ class Tester:
         #     assert False, 'Unknowk dataset'
         # return pd.read_pickle(os.path.join(os.environ['HOME'], f'Dataset/dataset2/konbini-stacked/bin_state{start_index+idx:05}.pkl'))
 
-        start_index = 1500
+        start_index = 1750
         return pd.read_pickle(os.path.join(os.environ['HOME'], f'Dataset/dataset2/basket-filling3/bin_state{start_index+idx:05}.pkl'))
 
     def show_forcemap(self, fmap_values, bin_state):
@@ -122,32 +111,7 @@ class Tester:
         viewer.publish_bin_state(bin_state, self._fmap)
 
 
-tester = Tester(test_data, model, params)
+tester = Tester(test_data, model, dataset_params)
 
 # tester.predict(idx)
 # tester.show_labels(idx)
-
-# model = ForceEstimationDINOv2(device='cpu')
-# model = ForceEstimationResNet(device='cpu')
-
-def plot_forcelabel(i):
-    fmap.set_values(force_labels[i])
-    bin_state = load_bin_state(i)
-    viewer.publish_bin_state(bin_state, fmap)
-
-
-# fig, ax = plt.subplots(1, 2, figsize=(8, 3), dpi=60)
-# def anim_update(i):
-#     for j in range(2):
-#         ax[j].cla()
-
-#     ax[0].imshow(images_raw[idx,i,:,:,::-1])
-#     ax[0].axis('off')
-#     ax[0].set_title('Input image')
-
-#     ax[1].imshow(yi[i,:,:,::-1])
-#     ax[1].axis('off')
-#     ax[1].set_title('Reconstructed image')
-
-# ani = anim.FuncAnimation(fig, anim_update, interval=int(T/10), frames=T)
-# ani.save( './output/{}_{}.gif'.format(params['model'], idx) )
