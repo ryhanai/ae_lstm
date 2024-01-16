ddfd67602befcd772d297b08ddf843a2ca78488c
Ryo Hanai <ryo.hanai@aist.go.jp>
2024-01-16 14:23:19 +0900

    bug fix

--- scripts/evaluation/create_ycb_dataset_for_yolo.py
+++ scripts/evaluation/create_ycb_dataset_for_yolo.py
@@ -22,6 +22,7 @@ def do_create():
             gen_polygon_mask(image_file, f"{to_label_dir}/{start_idx + i:012d}.txt")
         start_idx += len(image_files)
 
+    start_idx = 123705
     from_dir = YCB_VIDEO_DIR / "data_syn"
     image_files = glob.glob(str(from_dir / "*-color.jpg"))
     for i, image_file in enumerate(image_files):
@@ -41,3 +42,34 @@ def do_create():
             print(f"copy: {image_file}, {to_img_dir}/{start_idx + i:012d}.jpg")
             gen_polygon_mask(image_file, f"{to_label_dir}/{start_idx + i:012d}.txt")
         start_idx += len(image_files)
+
+
+def view_converted_polygons():
+    # start_idx = 0
+    # to_img_dir = YOLO_DATA_DIR / "images" / "train"
+    # for seq_num in range(80):
+    #     from_dir = YCB_VIDEO_DIR / "data" / f"{seq_num:04d}"
+    #     image_files = glob.glob(str(from_dir / "*-color.jpg"))
+    #     for i, image_file in enumerate(image_files):
+    #         print(f"{image_file}")
+    #         gen_polygon_mask(image_file, None, save_result=False, view_result=True)
+    #     start_idx += len(image_files)
+
+    from_dir = YCB_VIDEO_DIR / "data_syn"
+    image_files = glob.glob(str(from_dir / "*-color.jpg"))
+    for i, image_file in enumerate(image_files):
+        print(f"{image_file}")
+        gen_polygon_mask(image_file, None, save_result=False, view_result=True)
+    start_idx += len(image_files)
+
+    # start_idx = 0
+    # to_img_dir = YOLO_DATA_DIR / "images" / "test"
+    # to_label_dir = YOLO_DATA_DIR / "labels" / "test"
+    # for seq_num in range(80, 92):
+    #     from_dir = YCB_VIDEO_DIR / "data" / f"{seq_num:04d}"
+    #     image_files = glob.glob(str(from_dir / "*-color.jpg"))
+    #     for i, image_file in enumerate(image_files):
+    #         shutil.copy(image_file, f"{to_img_dir}/{start_idx + i:012d}.jpg")
+    #         print(f"copy: {image_file}, {to_img_dir}/{start_idx + i:012d}.jpg")
+    #         gen_polygon_mask(image_file, f"{to_label_dir}/{start_idx + i:012d}.txt")
+    #     start_idx += len(image_files)
--- scripts/evaluation/mask2polygon.py
+++ scripts/evaluation/mask2polygon.py
@@ -31,22 +31,24 @@ def read_color_image(sequence_number, frame_number):
     return cv2.imread(str(p))
 
 
-def draw_polygons(bgr_img, polygons, color=[0, 255, 0]):
-    rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+def annotate_polygons(rgb_img, polygons, color=[0, 255, 0]):
+    annotated_img = rgb_img
     for p in polygons:
-        annotated_img = sv.draw_polygon(rgb_img, p, sv.draw.color.Color(*color))
-    plt.imshow(annotated_img)
-    plt.show()
+        annotated_img = sv.draw_polygon(annotated_img, p, sv.draw.color.Color(*color))
+    return annotated_img
 
 
 def save_polygons(f, cls_index, polygons, mask):
     height, width = mask.shape
-    f.write(f"{cls_index[0]-1} ")
-    for polygon in polygons:
-        polygon = np.array(polygon) / [width, height]
-        for v in polygon:
-            f.write(f"{v[0]:0.6f} {v[1]:0.6f} ")
-    f.write("\n")
+    if len(polygons) > 0:
+        f.write(f"{cls_index-1} ")
+        for polygon in polygons:
+            polygon = np.array(polygon) / [width, height]
+            for v in polygon:
+                f.write(f"{v[0]:0.6f} {v[1]:0.6f} ")
+        f.write("\n")
+    else:
+        print("skip saving undetected class")
 
 
 # def convert(sequence_number, frame_number, draw_result=False):
@@ -67,13 +69,25 @@ def save_polygons(f, cls_index, polygons, mask):
 #             save_polygons(f, cls_index, polygons, mask)
 
 
-def gen_polygon_mask(color_file_path, polygon_file_path):
+def gen_polygon_mask(color_file_path, polygon_file_path, save_result=True, view_result=False):
     meta_data = scipy.io.loadmat(str.replace(color_file_path, "-color.jpg", "") + "-meta.mat")
     cls_indices = meta_data["cls_indexes"]
+    cls_indices = cls_indices.flatten().astype(np.uint8)
     mask = cv2.imread(str.replace(color_file_path, "-color.jpg", "") + "-label.png")[:, :, 0]
 
-    with open(polygon_file_path, "w") as f:
+    if save_result:
+        with open(polygon_file_path, "w") as f:
+            for cls_index in cls_indices:
+                single_mask = np.where(mask == cls_index, 1, 0)
+                polygons = sv.mask_to_polygons(single_mask)
+                save_polygons(f, cls_index, polygons, mask)
+
+    if view_result:
+        bgr_img = cv2.imread(color_file_path)
+        rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
         for cls_index in cls_indices:
             single_mask = np.where(mask == cls_index, 1, 0)
             polygons = sv.mask_to_polygons(single_mask)
-            save_polygons(f, cls_index, polygons, mask)
+            rgb_img = annotate_polygons(rgb_img, polygons)
+        plt.imshow(rgb_img)
+        plt.show()
